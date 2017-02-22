85e4a083029170facd4b4a64fa4dec6f5b19d363
n0099 <n00099@126.com>
2017-02-22 21:37:00 +0800

    简单的分页

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 'On');
-if (!is_int(intval($_GET['tid']))) { die(); }
+$_GET['tid'] = (int)$_GET['tid'];
+$_GET['pn'] = (int)$_GET['pn'];
 
 function get_post_portal($tid, $pid = null, $spid = null) {
     $return = "http://tieba.baidu.com/p/{$tid}";
@@ -27,7 +28,7 @@ $sql = new mysqli('127.0.0.1', 'n0099', 'iloven0099', 'n0099');
         <div class="container">
             <div class="row clearfix">
                 <div class="col-md-12 column">
-                    <p><?php echo empty($_GET['tid']) ? '显示最近150条主题贴/回复贴/楼中楼记录' : '显示此<a href="'.get_post_portal($_GET['tid']).'" target="_blank">主题贴</a>所有回复贴楼中楼记录'; ?></p>
+                    <p><?php echo empty($_GET['tid']) ? '显示最近30条主题贴/回复贴/楼中楼记录' : '显示此<a href="' . get_post_portal($_GET['tid']) . '" target="_blank">主题贴</a>所有回复贴楼中楼记录'; ?></p>
                     <table class="table table-hover table-striped table-condensed" id="main">
                         <thead>
                             <tr>
@@ -41,15 +42,22 @@ $sql = new mysqli('127.0.0.1', 'n0099', 'iloven0099', 'n0099');
                         </thead>
                         <tbody>
                             <?php
-                            $sql_posts = empty($_GET['tid']) ? 'SELECT * FROM tbmonitor_post ORDER BY post_time DESC LIMIT 50' : "SELECT * FROM tbmonitor_post WHERE tid = {$_GET['tid']}";
+                            $count = $sql -> query('SELECT COUNT(*) FROM tbmonitor_post UNION ALL SELECT COUNT(*) FROM tbmonitor_reply WHERE floor != 1 UNION ALL SELECT COUNT(*) FROM tbmonitor_lzl') -> fetch_all(MYSQLI_NUM);
+                            foreach ($count as $key => $value) {
+                                $pages += $value[0];
+                            }
+                            $pages =  $pages / 30;
+                            $sql_limit = "LIMIT {$_GET['pn']}, " . ($_GET['pn'] + 10);
+                            echo $sql_limit;
+                            $sql_posts = empty($_GET['tid']) ? "SELECT * FROM tbmonitor_post ORDER BY post_time DESC {$sql_limit}" : "SELECT * FROM tbmonitor_post WHERE tid = {$_GET['tid']}";
                             $sql_posts_result = $sql -> query($sql_posts) -> fetch_all(MYSQLI_ASSOC);
-                            $sql_replies = empty($_GET['tid']) ? 'SELECT * FROM tbmonitor_reply WHERE floor != 1 ORDER BY reply_time DESC LIMIT 50' : "SELECT * FROM tbmonitor_reply WHERE tid = {$_GET['tid']} AND floor != 1";
+                            $sql_replies = empty($_GET['tid']) ? "SELECT * FROM tbmonitor_reply WHERE floor != 1 ORDER BY reply_time DESC {$sql_limit}" : "SELECT * FROM tbmonitor_reply WHERE tid = {$_GET['tid']} AND floor != 1";
                             $sql_replies_result = $sql -> query($sql_replies) -> fetch_all(MYSQLI_ASSOC);
-                            $sql_lzl = empty($_GET['tid']) ? 'SELECT * FROM tbmonitor_lzl ORDER BY reply_time DESC LIMIT 50' : "SELECT * FROM tbmonitor_lzl WHERE tid = {$_GET['tid']}";
-                            $sql_lzl_result = $sql -> query($sql_lzl) -> fetch_all(MYSQLI_ASSOC);                            
+                            $sql_lzl = empty($_GET['tid']) ? "SELECT * FROM tbmonitor_lzl ORDER BY reply_time DESC {$sql_limit}" : "SELECT * FROM tbmonitor_lzl WHERE tid = {$_GET['tid']}";
+                            $sql_lzl_result = $sql -> query($sql_lzl) -> fetch_all(MYSQLI_ASSOC);
                             foreach ($sql_posts_result as $post) {
-                                $post['post_time'] = date('Y-m-d H:i', $post['post_time']);
-                                $post['latest_reply_time'] = date('Y-m-d H:i', $post['latest_reply_time']);
+                                $post['post_time'] = date('Y-m-d H:i', strtotime($post['post_time']));
+                                $post['latest_reply_time'] = date('Y-m-d H:i', strtotime($post['latest_reply_time']));
                             ?>
                                 <tr>
                                     <td><?php echo $post['forum']; ?></th>
@@ -61,52 +69,71 @@ $sql = new mysqli('127.0.0.1', 'n0099', 'iloven0099', 'n0099');
                                         <p id=<?php echo "\"post_{$post['tid']}\""; ?> class="collapse out">
                                             <?php echo $sql -> query("SELECT content FROM tbmonitor_reply WHERE tid = {$post['tid']} AND floor = 1") -> fetch_assoc()['content']; ?>
                                         </p>
-                                        <?php echo "主题贴回复数：{$post['reply_num']} 最后回复人：<a href=\"".get_user_space($post['latest_replyer'])."\" target=\"_blank\">{$post['latest_replyer']}</a> 最后回复时间：{$post['latest_reply_time']}"; ?>
+                                        <?php echo "主题贴回复数：{$post['reply_num']} 最后回复人：<a href=\"" . get_user_space($post['latest_replyer']) . "\" target=\"_blank\">{$post['latest_replyer']}</a> 最后回复时间：{$post['latest_reply_time']}"; ?>
                                     </th>
-                                    <td><?php echo '<a href="'.get_post_portal($post['tid']).'" target="_blank">传送门</a>'; ?></th>
-                                    <td><?php echo '<a href="'.get_user_space($post['author'])."\" target=\"_blank\">{$post['author']}</a>"; ?></th>
+                                    <td><?php echo '<a href="' . get_post_portal($post['tid']) . '" target="_blank">传送门</a>'; ?></th>
+                                    <td><?php echo '<a href="' . get_user_space($post['author']) . "\" target=\"_blank\">{$post['author']}</a>"; ?></th>
                                     <td><?php echo $post['post_time']; ?></th>
                                 </tr>
                             <?php }
                             foreach ($sql_replies_result as $reply) {
-                                $reply['reply_time'] = date('Y-m-d H:i', $reply['reply_time']);
+                                $reply['reply_time'] = date('Y-m-d H:i', strtotime($reply['reply_time']));
                             ?>
                                 <tr>
                                     <td><?php echo $reply['forum']; ?></th>
                                     <td>回复贴</th>
-                                    <td><?php echo '所回复主题贴：<a href="'.get_post_portal($reply['tid']).'" target="_blank">'.$sql -> query("SELECT title FROM tbmonitor_post WHERE tid = {$reply['tid']}") -> fetch_assoc()['title'].'</a><br />'; ?>
+                                    <td><?php echo '所回复主题贴：<a href="' . get_post_portal($reply['tid']) . '" target="_blank">' . $sql -> query("SELECT title FROM tbmonitor_post WHERE tid = {$reply['tid']}") -> fetch_assoc()['title'] . '</a><br />'; ?>
                                         <a data-toggle="collapse" data-target=<?php echo "\"#reply_{$reply['pid']}\""; ?> href="">
                                             点击展开回复
                                         </a><br />
                                         <p id=<?php echo "\"reply_{$reply['pid']}\""; ?> class="collapse out">
-                                            <?php echo $reply['content'].'<br />'; ?>
+                                            <?php echo $reply['content'] . '<br />'; ?>
                                         </p>
                                         <?php echo "楼层：{$reply['floor']} 楼中楼回复数：{$reply['lzl_num']}"; ?>
                                     </th>
-                                    <td><?php echo '<a href="'.get_post_portal($reply['tid'], $reply['pid']).'" target="_blank">传送门</a>'; ?></th>
-                                    <td><?php echo '<a href="'.get_user_space($reply['author'])."\" target=\"_blank\">{$reply['author']}</a>"; ?></th>
+                                    <td><?php echo '<a href="' . get_post_portal($reply['tid'], $reply['pid']) . '" target="_blank">传送门</a>'; ?></th>
+                                    <td><?php echo '<a href="' . get_user_space($reply['author']) . "\" target=\"_blank\">{$reply['author']}</a>"; ?></th>
                                     <td><?php echo $reply['reply_time']; ?></th>
                                 </tr>
                             <?php }
                             foreach ($sql_lzl_result as $lzl) {
-                                $lzl['reply_time'] = date('Y-m-d H:i', $lzl['reply_time']);
+                                $lzl['reply_time'] = date('Y-m-d H:i', strtotime($lzl['reply_time']));
                             ?>
                                 <tr>
                                     <td><?php echo $lzl['forum']; ?></th>
                                     <td>楼中楼</th>
                                     <td><?php
-                                        echo '所回复主题贴：<a href="'.get_post_portal($lzl['tid']).'" target="_blank">'.$sql -> query("SELECT title FROM tbmonitor_post WHERE tid = {$lzl['tid']}") -> fetch_assoc()['title'].'</a>';
-                                        echo ' 所回复楼层：<a href="'.get_post_portal($lzl['tid'], $lzl['pid']).'" target="_blank">'.$sql -> query("SELECT floor FROM tbmonitor_reply WHERE pid = {$lzl['pid']}") -> fetch_assoc()['floor'].'楼</a><br />';
+                                        echo '所回复主题贴：<a href="' . get_post_portal($lzl['tid']) . '" target="_blank">' . $sql -> query("SELECT title FROM tbmonitor_post WHERE tid = {$lzl['tid']}") -> fetch_assoc()['title'] . '</a>';
+                                        echo ' 所回复楼层：<a href="' . get_post_portal($lzl['tid'], $lzl['pid']) . '" target="_blank">' . $sql -> query("SELECT floor FROM tbmonitor_reply WHERE pid = {$lzl['pid']}") -> fetch_assoc()['floor'] . '楼</a><br />';
                                         echo $lzl['content'];
                                     ?></th>
-                                    <td><?php echo '<a href="'.get_post_portal($lzl['tid'], $lzl['pid'], $lzl['spid']).'" target="_blank">传送门</a>'; ?></th>
-                                    <td><?php echo '<a href="'.get_user_space($lzl['author'])."\" target=\"_blank\">{$lzl['author']}</a>"; ?></th>
+                                    <td><?php echo '<a href="' . get_post_portal($lzl['tid'], $lzl['pid'], $lzl['spid']) . '" target="_blank">传送门</a>'; ?></th>
+                                    <td><?php echo '<a href="' . get_user_space($lzl['author'])  .  "\" target=\"_blank\">{$lzl['author']}</a>"; ?></th>
                                     <td><?php echo $lzl['reply_time']; ?></th>
                                 </tr>
                             <?php } ?>
                         </tbody>
                     </table>
-                    <p><?php echo 'PHP耗时'.round(microtime(true)-$time, 10).'秒，共使用'.round(memory_get_peak_usage()/1024/1024, 2).'MB内存'; ?></p>
+                    <nav>
+                        <ul class="pagination justify-content-end">
+                            <?php
+                            $pre_class = $_GET['pn'] == 0 ? '"page-item disabled"' : '"page-item"';
+                            $pre_href = $_GET['pn'] == 0 ? '""' : '"https://n0099.cf/tbm/?pn=' . ($_GET['pn'] - 10) . '"';
+                            $next_class = $_GET['pn'] == intval($pages) + 1 * 10 ? '"page-item disabled"' : '"page-item"';
+                            $next_href = $_GET['pn'] == intval($pages) + 1 * 10 ? '""' : '"https://n0099.cf/tbm/?pn=' . ($_GET['pn'] + 10) . '"';
+                            ?>
+                            <li class=<?php echo $pre_class; ?>><a class="page-link" href=<?php echo $pre_href; ?>>上一页</a></li>
+                            <?php
+                            for ($i = 1; $i <= intval($pages) + 1; $i++) {
+                                if ($i == 10) { break; }
+                                $li_class = $_GET['pn'] == $i * 10 ? 'page-item active' : 'page-item';
+                                echo "<li class=\"{$li_class}\">".'<a class="page-link" href="https://n0099.cf/tbm/?pn=' . $i * 10 . '">' . $i . '</a></li>';
+                            }
+                            ?>
+                            <li class=<?php echo $next_class; ?>><a class="page-link" href=<?php echo $next_href; ?>>下一页</a></li>
+                        </ul>
+                    </nav>
+                    <p><?php echo 'PHP耗时' . round(microtime(true)-$time, 10) . '秒，共使用' . round(memory_get_peak_usage()/1024/1024, 2) . 'MB内存'; ?></p>
                 </div>
             </div>
         </div>
