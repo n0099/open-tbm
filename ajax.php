2779e2a73dd5866aaf70e5e31888e818583988c0
n0099 <n00099@126.com>
2017-03-02 17:26:20 +0800

    格式化

--- ajax.php
+++ ajax.php
@@ -8,7 +8,7 @@ $_GET['forum'] = $sql -> escape_string($_GET['forum']);
 $_GET['post'] = $sql -> escape_string($_GET['post']);
 $_GET['days'] = (int)$_GET['days'];
 if ($_GET['post'] !== 'post' & $_GET['post'] !== 'reply' & $_GET['post'] !== 'lzl') { $_GET['post'] == null; }
-$time_column_name = $_GET['post'] == 'reply' || $_GET['post'] == 'lzl' ? 'reply_time' : 'post_time'; 
+$time_column_name = $_GET['post'] == 'reply' || $_GET['post'] == 'lzl' ? 'reply_time' : 'post_time';
 
 switch ($_GET['type']) {
     case 'get_cron_time':
@@ -19,8 +19,7 @@ switch ($_GET['type']) {
         break;
     case 'get_post_count_by_day':
         echo json_encode($sql -> query("SELECT CAST({$time_column_name} AS DATE) AS DATE, COUNT(*) FROM tbmonitor_{$_GET['post']} WHERE forum = \"{$_GET['forum']}\" AND DATE_SUB(CURDATE(), INTERVAL {$_GET['days']} DAY) <= date({$time_column_name}) GROUP BY CAST({$time_column_name} AS DATE)") -> fetch_all(MYSQLI_ASSOC));
+        break;
     default:
         die();
-        break;
 }
-?>
