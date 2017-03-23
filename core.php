e7a03dbf32a0c3a63cff75899a91def8b0aab41f
n0099 <n00099@126.com>
2017-03-23 17:48:52 +0800

    新建 core.php

---
+++ core.php
@@ -0,0 +1,31 @@
+<?php
+ini_set('display_errors', 'On');
+date_default_timezone_set('PRC');
+$sql = new mysqli('127.0.0.1', 'n0099', 'iloven0099', 'n0099');
+$sql -> query("SET collation_connection = utf8mb4_unicode_ci");
+
+function get_cron_time($minutes, $get_value) {
+    $value = round($GLOBALS['sql'] -> query("SELECT AVG(time) FROM tbmonitor_time WHERE date >= DATE_ADD(NOW(), INTERVAL -{$minutes} MINUTE)") -> fetch_all(MYSQLI_ASSOC)[0]['AVG(time)'], 2);
+    if ($get_value == false) { return empty($value) ? '未知' : $value; }
+    switch ($value) {
+        case $value >= 60:
+            return '低';
+        case $value >= 30:
+            return '中';
+        case $value < 30:
+            return '高';
+        default:
+            return '未知';
+    }
+}
+
+function get_post_portal($tid, $pid = null, $spid = null) {
+    $return = "http://tieba.baidu.com/p/{$tid}";
+    $return = $pid != null & $spid == null ? "{$return}?pid={$pid}#{$pid}" : $return;
+    $return = $pid != null & $spid != null ? "{$return}?pid={$pid}&cid={$spid}#{$spid}" : $return;
+    return $return;
+}
+
+function get_user_space($username) {
+    return "http://tieba.baidu.com/home/main?un={$username}&ie=utf-8";
+}
