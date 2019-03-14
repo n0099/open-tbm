a59084821f9bde6b4a016942889816626ecc0467
n0099 <n@n0099.net>
2019-03-14 20:26:02 +0800

    fix wrong query params validate require relations in posts query controller disable reCAPTCHA when in development environment

--- app/Http/Middleware/ReCAPTCHACheck.php
+++ app/Http/Middleware/ReCAPTCHACheck.php
@@ -6,13 +6,17 @@ class ReCAPTCHACheck
 {
     public function handle(\Illuminate\Http\Request $request, \Closure $next)
     {
-        $reCAPTCHA = new \ReCaptcha\ReCaptcha(env('reCAPTCHA_SECRET_KEY'));
-        $requestReCAPTCHA = $request->input('reCAPTCHA');
-        $isReCAPTCHAValid = $requestReCAPTCHA == null ? false : $reCAPTCHA->verify($requestReCAPTCHA, $request->ip())->isSuccess();
-        if ($isReCAPTCHAValid) {
-            return $next($request);
+        if (\App::environment('production')) {
+            $reCAPTCHA = new \ReCaptcha\ReCaptcha(env('reCAPTCHA_SECRET_KEY'));
+            $requestReCAPTCHA = $request->input('reCAPTCHA');
+            $isReCAPTCHAValid = $requestReCAPTCHA == null ? false : $reCAPTCHA->verify($requestReCAPTCHA, $request->ip())->isSuccess();
+            if ($isReCAPTCHAValid) {
+                return $next($request);
+            } else {
+                abort(400);
+            }
         } else {
-            abort(400);
+            return $next($request);
         }
     }
 }
