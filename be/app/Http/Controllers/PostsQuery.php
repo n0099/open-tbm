n0099/open-tbm

commit df6e42326a00b3d08feb2b970ecd681c91801b75

* allow gender, name and displayName params being set to 'NULL' to query these field with `IS NULL` sql @ app/Http/Controllers/UsersQuery.php * stop using `{!! !!}` to prevent stored xss vulnerability @ resources/views/formatPostJsonContent.blade.php @ be + add (display)nameNull option in selectBy as a shortcut to set param value with 'NULL' @ components/SelectTiebaUser.vue + import 'lazysizes' for lazyloading portraits in <UserList> @ main.ts + add notyShow() and routeNameStrAssert() helper @ shared/index.ts + add types Pagination, TiebaUserGender(QP), TiebaUserInfo, ApiUsersQuery(QP) @ api/index.d.ts * rename ApiStatsQP to ApiStatsForumPostsCountQP @ api/index.d.ts * fix thrown exception.errorCode is hardcoded `0` @ api/index.ts * change coding style of `(async ()).finally(cb)` to `.finally(cb)` @ views/BilibiliVote|Stat(u)s.vue * components/UserList.vue and views/User.vue is now fully operational * rename components/PlaceholderLoadingList.vue to PlaceholderPostList.vue with addition of isLoading prop * rename components/PlaceholderError404.vue to PlaceholderError.vue, since now it can render object with type ApiError - remove components/UserListPages.vue, fetch logic is now in views/User.vue @ fe

be/resources/views/formatPostJsonContent.blade.php
@@ -7,7 +7,7 @@ try {
     @foreach ($content as $item)
         @switch ($item['type'])
             @case (0) {{--文本 {"text": "content\n", "type": "0"} --}}
-                <span>{!! nl2br(trim($item['text'], "\n")) !!}</span>
+                <span>{{ nl2br(trim($item['text'], "\n")) }}</span>
                 @break
             @case (1)
                 {{--链接

be/app/Http/Controllers/UsersQuery.php
@@ -15,13 +15,24 @@ class UsersQuery extends Controller
             'uid' => 'integer',
             'name' => 'string',
             'displayName' => 'string',
-            'gender' => 'integer|in:0,1,2'
+            'gender' => 'string|in:0,1,2,NULL'
         ]);
         Helper::abortAPIIf(40402, empty($queryParams));
 
-        $queriedInfo = UserModel::where($queryParams)
+        $queryBuilder = (new UserModel())->newQuery();
+
+        $nullableParams = ['name', 'displayName', 'gender'];
+        foreach ($nullableParams as $nullableParamName) {
+            if (array_key_exists($nullableParamName, $queryParams) && $queryParams[$nullableParamName] === 'NULL') {
+                $queryBuilder = $queryBuilder->whereNull($nullableParamName);
+                unset($queryParams[$nullableParamName]);
+            }
+        }
+
+        $queriedInfo = $queryBuilder->where($queryParams)
             ->orderBy('id', 'DESC')
-            ->hidePrivateFields()->simplePaginate($this->pagingPerPageItems);
+            ->hidePrivateFields()
+            ->simplePaginate($this->pagingPerPageItems);
         Helper::abortAPIIf(40402, $queriedInfo->isEmpty());
 
         return [

fe/src/api/index.d.ts
@@ -1,3 +1,4 @@
+import type { SelectTiebaUserParams } from '@/components/SelectTiebaUser.vue';
 import type { Float, UInt, UnixTimestamp } from '@/shared';
 import type { Mix } from '@/shared/groupBytimeGranularityUtcPlus8';
 export interface ApiError { errorCode: number, errorInfo: Record<string, unknown[]> | string }
@@ -31,9 +32,27 @@ export interface ApiStatsForumPostsCount {
     reply: TimeCountPair[],
     subReply: TimeCountPair[]
 }
-export interface ApiStatsQP extends ApiQueryParam {
+export interface ApiStatsForumPostsCountQP extends ApiQueryParam {
     fid: UInt,
     timeGranularity: 'day' | 'hour' | 'minute' | 'month' | 'week' | 'year',
     startTime: UnixTimestamp,
     endTime: UnixTimestamp
 }
+
+export type Pagination = { [P in 'currentItems' | 'currentPage' | 'firstItem']: number };
+export type TiebaUserGender = 0 | 1 | 2 | null;
+export type TiebaUserGenderQP = '0' | '1' | '2' | 'NULL';
+export interface TiebaUserInfo {
+    uid: number,
+    avatarUrl: string,
+    name: string | null,
+    displayName: string | null,
+    fansNickname: string | null,
+    gender: TiebaUserGender,
+    iconInfo: Array<Record<string, unknown>> | null
+}
+export interface ApiUsersQuery {
+    pages: Pagination,
+    users: TiebaUserInfo[]
+}
+export type ApiUsersQueryQP = ApiQueryParam & SelectTiebaUserParams & { gender?: TiebaUserGenderQP };

fe/src/api/index.ts
@@ -1,24 +1,26 @@
-import type { ApiError, ApiForumList, ApiQueryParam, ApiStatsForumPostsCount, ApiStatsQP, ApiStatus, ApiStatusQP } from '@/api/index.d';
+import type { ApiError, ApiForumList, ApiQueryParam, ApiStatsForumPostsCount, ApiStatsForumPostsCountQP, ApiStatus, ApiStatusQP, ApiUsersQuery, ApiUsersQueryQP } from '@/api/index.d';
+import { notyShow } from '@/shared';
 import NProgress from 'nprogress';
 import qs from 'qs';
 import _ from 'lodash';
-import Noty from 'noty';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-export const isApiError = (r: any): r is ApiError => 'errorInfo' in r && _.isString(r.errorInfo);
+export const isApiError = <T>(r: ApiError | T): r is ApiError => 'errorInfo' in r && _.isString(r.errorInfo);
 export const nullIfApiError = <T>(api: ApiError | T): T | null => (isApiError(api) ? null : api);
 export const getRequester = async <T = ApiError | unknown>(endpoint: string, queryString?: ApiQueryParam): Promise<ApiError | T> => {
     NProgress.start();
     document.body.style.cursor = 'progress';
+    let errorCode = 0;
     let errorMessage = `GET ${endpoint}<br />`;
     try {
         const response = await fetch(
             process.env.VUE_APP_API_URL_PREFIX + endpoint + (_.isEmpty(queryString) ? '' : '?') + qs.stringify(queryString),
             { headers: { Accept: 'application/json' } }
         );
+        errorCode = response.status;
         errorMessage += `HTTP ${response.status} `;
         const json = await response.json() as T;
         if (isApiError(json)) {
+            ({ errorCode } = json);
             errorMessage += `错误码：${json.errorCode}<br />`;
             if (_.isObject(json.errorInfo)) {
                 errorMessage += _.map(json.errorInfo, (info, paramName) =>
@@ -33,8 +35,9 @@ export const getRequester = async <T = ApiError | unknown>(endpoint: string, que
     } catch (e: unknown) {
         if (e instanceof Error) {
             const { message: exceptionMessage } = e;
-            new Noty({ timeout: 3000, type: 'error', text: `${errorMessage}<br />${exceptionMessage}` }).show();
-            return { errorCode: 0, errorInfo: exceptionMessage };
+            const text = `${errorMessage}<br />${exceptionMessage}`;
+            notyShow('error', text);
+            return { errorCode, errorInfo: text.replaceAll('<br />', '\n') };
         }
         throw e;
     } finally {
@@ -64,5 +67,7 @@ export const apiForumList = async (): Promise<ApiError | ApiForumList> =>
     getRequester('/forumList');
 export const apiStatus = async (qp: ApiStatusQP): Promise<ApiError | ApiStatus> =>
     getRequesterWithReCAPTCHA<ApiStatus>('/status', qp);
-export const apiStatsForumPostsCount = async (qp: ApiStatsQP): Promise<ApiError | ApiStatsForumPostsCount> =>
+export const apiStatsForumPostsCount = async (qp: ApiStatsForumPostsCountQP): Promise<ApiError | ApiStatsForumPostsCount> =>
     getRequesterWithReCAPTCHA<ApiStatsForumPostsCount>('/stats/forumPostsCount', qp);
+export const apiUsersQuery = async (qp: ApiUsersQueryQP): Promise<ApiError | ApiUsersQuery> =>
+    getRequesterWithReCAPTCHA<ApiUsersQuery>('/usersQuery', qp);

fe/src/components/PlaceholderError.vue
@@ -1,0 +1,30 @@
+<template>
+    <div class="text-center">
+        <hr />
+        <template v-if="error === null">
+            <span class="errorCode text-muted">error</span>
+        </template>
+        <template v-else>
+            <p class="errorCode text-muted">{{ error.errorCode }}</p>
+            <p v-for="(info, key) in error.errorInfo.split('\n')" :key="key">{{ info }}</p>
+        </template>
+    </div>
+</template>
+
+<script lang="ts">
+import type { ApiError } from '@/api/index.d';
+import type { PropType } from 'vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        error: { type: [Object, null] as PropType<ApiError | null>, required: true }
+    }
+});
+</script>
+
+<style scoped>
+.errorCode {
+    font-size: 6em;
+}
+</style>

fe/src/components/PlaceholderError404.vue
@@ -1,15 +1,0 @@
-<template>
-    <div class="placeholderError404 text-center">
-        <hr />404
-    </div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped>
-.placeholderError404 {
-    font-size: 8em;
-}
-</style>

fe/src/components/PlaceholderPostList.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="row align-items-center">
-        <div class="col"><hr /></div>
-        <div class="w-auto">
-            <div class="loading mx-auto"></div>
-        </div>
+        <template v-if="isLoading">
+            <div class="col"><hr /></div>
+            <div class="w-auto">
+                <div class="loading mx-auto"></div>
+            </div>
+        </template>
         <div class="col"><hr /></div>
         <div class="w-100"></div>
         <div class="col">
@@ -13,7 +15,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        isLoading: {
+            type: Boolean,
+            default: true
+        }
+    }
+};
 </script>
 
 <style scoped>

commit 40ac88a1c92251c05b5a004f183b0e345f7cbcb8

* now will ignore vary values of the param postTypes when determining $isPostIDQuery, sync with behaviour @ fe/src/components/Post/QueryForm.vue:L207 * now won't add the verbose clause: `WHERE postTypes IN ('thread', 'reply', 'subReply')` @ IndexQuery.php
* add method omit() as a inverse of filter()
* fix default subParam of orderBy will not be added @ addDefaultValueOnUniqueParams()
@ QueryParams.php
@ app/Http/PostsQuery

* fix `<br />` tag created by nl2br() will be escaped to html entities, it's regressed by df6e42326a00b3d08feb2b970ecd681c91801b75 @ case(0)
* extract original url behind `http://tieba.baidu.com/mo/q/checkurl?url=` @ case(1)
* fix video will be only rendered as a link to video play page @ case(5)
@ resources/views/formatPostJsonContent.blade.php
@ be

* add prefix `get` before name of currentQueryType()
* fix doesn't check required orderBy when query type is postID @ checkParams()
* fix issueing query emit before checkParams() returned @ submit()
@ QueryForm.vue
- remove optional spread parameter `omitParams` of clearedUniqueParamsDefaultValue() @ useQueryForm.ts
@ fe/src/components/Post

be/app/Http/Controllers/PostsQuery.php
@@ -16,17 +16,21 @@ class PostsQuery extends Controller
 
     public function query(\Illuminate\Http\Request $request): array
     {
-        $validator = new ParamsValidator((array)Utils::jsonDecode($request->validate([
-            'page' => 'integer',
-            'query' => 'json'
-        ])['query'], true));
+        $validator = new ParamsValidator((array)Utils::jsonDecode(
+            $request->validate([
+                'page' => 'integer',
+                'query' => 'json'
+            ])['query'],
+            true
+        ));
         $params = $validator->params;
 
         $postsIDParams = $params->filter(...Helper::POSTS_ID);
-        $isPostIDQuery = $params->count() === \count($postsIDParams) // is there no other params
+        $isPostIDQuery = // is there no other params, note we ignored the existence of unique params
+            \count($params->omit('postTypes')) === \count($postsIDParams)
             && \count($postsIDParams) === 1 // is there only one post id param
             && array_filter($postsIDParams, static fn ($p) => $p->getAllSub() !== []) === []; // is post id param haven't any sub param
-        // is fid param exists and there's no other params
+        // is the fid param exists and there's no other params
         $isFidParamNull = $params->getUniqueParamValue('fid') === null;
         $isFidQuery = !$isFidParamNull && $params->count() === \count($params->filter(...ParamsValidator::UNIQUE_PARAMS_NAME));
         $isIndexQuery = $isPostIDQuery || $isFidQuery;
@@ -37,11 +41,8 @@ class PostsQuery extends Controller
 
         $validator->addDefaultParamsThenValidate();
 
-        if ($isSearchQuery) {
-            $query = (new SearchQuery($this->perPageItems))->query($params);
-        } else {
-            $query = (new IndexQuery($this->perPageItems))->query($params);
-        }
+        $queryClass = $isIndexQuery ? IndexQuery::class : SearchQuery::class;
+        $query = (new $queryClass($this->perPageItems))->query($params);
         $queryResult = $query->fillWithParentPost();
 
         return [

be/app/Http/PostsQuery/IndexQuery.php
@@ -17,22 +17,25 @@ class IndexQuery
             static fn (array $accParams, Param $param) => array_merge($accParams, [$param->name => $param->value]),
             []
         ); // flatten unique query params
-        $indexQuery = IndexModel::where(Arr::only($flatQueryParams, ['fid', ...Helper::POSTS_ID]));
+
+        $query = IndexModel::where(Arr::only($flatQueryParams, ['fid', ...Helper::POSTS_ID]));
         if ($flatQueryParams['orderBy'] !== 'default') {
-            $indexQuery->orderBy($flatQueryParams['orderBy'], $flatQueryParams['direction']);
+            $query->orderBy($flatQueryParams['orderBy'], $flatQueryParams['direction']);
         } elseif (Arr::only($flatQueryParams, Helper::POSTS_ID) === []) { // query by fid only
-            $indexQuery->orderByDesc('postTime'); // order by postTime to prevent posts out of order when order by post id
+            $query->orderByDesc('postTime'); // order by postTime to prevent posts out of order when order by post id
         } else { // query by post id
             // order by all posts id to keep reply and sub reply continuous instated of clip into multi-page since they are varied in postTime
-            $indexQuery->orderByMulti(array_fill_keys(Helper::POSTS_ID, 'ASC'));
+            $query->orderByMulti(array_fill_keys(Helper::POSTS_ID, 'ASC'));
         }
-
-        $result = $indexQuery->whereIn('type', $flatQueryParams['postTypes'])->simplePaginate($this->perPageItems);
+        if ($flatQueryParams['postTypes'] !== Arr::sort(Helper::POST_TYPES)) {
+            $query->whereIn('type', $flatQueryParams['postTypes']);
+        }
+        $result = $query->simplePaginate($this->perPageItems);
         Helper::abortAPIIf(40401, $result->isEmpty());
 
         $this->queryResult = array_merge(
             ['fid' => $result->pluck('fid')->first()],
-            array_combine(Helper::POST_TYPES_PLURAL, array_map( // assign queried posts id from $indexQuery
+            array_combine(Helper::POST_TYPES_PLURAL, array_map( // assign queried posts id from $query
                 static fn ($postType) => $result->where('type', $postType)->toArray(),
                 Helper::POST_TYPES
             ))

be/app/Http/PostsQuery/ParamsValidator.php
@@ -96,7 +96,7 @@ class ParamsValidator
         $required[1] = Arr::sort($required[1]);
         return $required[0] === 'SUB'
             ? array_diff($current, $required[1]) === []
-            : $current ===$required[1];
+            : $current === $required[1];
     }
 
     protected function validate40003(): void

be/app/Http/PostsQuery/QueryParams.php
@@ -36,6 +36,17 @@ class QueryParams
         ));
     }
 
+    /**
+     * @return Param[]
+     */
+    public function omit(string ...$names): array
+    {
+        return array_values(array_filter(
+            $this->params,
+            static fn ($p): bool => !\in_array($p->name, $names, true)
+        ));
+    }
+
     public function getUniqueParamValue(string $name): mixed
     {
         return $this->filter($name)[0]->value ?? null;
@@ -56,13 +67,13 @@ class QueryParams
             'postTypes' => ['value' => Helper::POST_TYPES],
             'orderBy' => ['value' => 'default', 'subParam' => ['direction' => 'default']]
         ];
-        foreach ($uniqueParamsDefaultValue as $uniqueParamName => $uniqueParamDefaultValue) {
+        foreach ($uniqueParamsDefaultValue as $name => $value) {
             // add unique params with default value when it's not presented in $this->params
-            if ($this->getUniqueParamValue($uniqueParamName) === null) {
-                $this->params[] = new Param(array_merge(
-                    [$uniqueParamName => $uniqueParamDefaultValue['value']],
-                    $uniqueParamDefaultValue['subParam'] ?? []
-                ));
+            if ($this->getUniqueParamValue($name) === null) {
+                $this->params[] = new Param([
+                    $name => $value['value'],
+                    'subParam' => $value['subParam'] ?? []
+                ]);
             }
         }
     }
@@ -96,7 +107,7 @@ class QueryParams
             ]
         ];
         $subParamsDefaultValue = collect($paramsNameByType)->flatMap(static fn (array $names, string $type) =>
-        array_fill_keys($names, $paramDefaultValueByType[$type]))->toArray();
+            array_fill_keys($names, $paramDefaultValueByType[$type]))->toArray();
         foreach ($this->params as $param) { // set sub params with default value
             foreach ($subParamsDefaultValue[$param->name] ?? [] as $name => $value) {
                 if ($param->getSub($name) === null) {

be/app/Http/PostsQuery/SearchQuery.php
@@ -171,11 +171,11 @@ class SearchQuery
             // not (A or B) <=> not A and not B, following https://en.wikipedia.org/wiki/De_Morgan%27s_laws
             $notOrWhere = $notString === 'Not' ? '' : 'or';
             $addMatchKeyword = static fn (string $keyword) =>
-            $query->{"{$notOrWhere}Where"}(
-                $fieldName,
-                "{$notString} LIKE",
-                $param['matchBy'] === 'implicit' ? "%{$keyword}%" : $keyword
-            );
+                $query->{"{$notOrWhere}Where"}(
+                    $fieldName,
+                    "{$notString} LIKE",
+                    $param['matchBy'] === 'implicit' ? "%{$keyword}%" : $keyword
+                );
             if ($param['spaceSplit']) {
                 foreach (explode(' ', $paramValue) as $splitedKeyword) { // split multiple search keyword by space char
                     $addMatchKeyword($splitedKeyword);

be/resources/views/formatPostJsonContent.blade.php
@@ -7,7 +7,7 @@ try {
     @foreach ($content as $item)
         @switch ($item['type'])
             @case (0) {{--文本 {"text": "content\n", "type": "0"} --}}
-                <span>{{ nl2br(trim($item['text'], "\n")) }}</span>
+                <span>{!! nl2br(htmlspecialchars(trim($item['text'], "\n"))) !!}</span>
                 @break
             @case (1)
                 {{--链接
@@ -16,7 +16,10 @@ try {
                     {"link": "http://tieba.baidu.com/mo/q/checkurl?url=", "text": "[失效] http://pan.baidu.com/s/", "type": "1", "url_type": "1"}
                     {"link": "http://tieba.baidu.com/mo/q/checkurl?url=", "text": "[有效] http://pan.baidu.com/s/", "type": "1", "url_type": "2"}
                 --}}
-                <a href="{{ $item['link'] }}" target="_blank">{{ $item['text'] }}</a>
+                <?php
+                    $skipCheckUrl = rawurldecode(Regex::match('@^http://tieba\.baidu\.com/mo/q/checkurl\?url=(.+?)(&|$)@', $item['link'])->group(1));
+                ?>
+                <a href="{{ empty($skipCheckUrl) ? $item['link'] : $skipCheckUrl }}" target="_blank">{{ $item['text'] }}</a>
                 @break
             @case (2) {{--表情 {"c": "滑稽", "text": "image_emoticon25", "type": "2"} --}}
                 <?php
@@ -102,17 +105,15 @@ try {
                         "is_native_app": "0"
                     }
                 --}}
-                <a href="{{ $item['link'] }}" target="_blank">
-                    @if (isset($item['origin_src']))
-                        <div class="tieba-image-zoom-in">
-                            <img class="tieba-image lazyload" referrerpolicy="no-referrer" data-src="{{ $item['origin_src'] }}" />
-                        </div>
-                    @else
-                        外站视频：{{ $item['text'] }}
-                    @endif
-                </a>
+                @if (isset($item['src']))
+                    <video controls poster="{{ $item['src'] }}" src="{{ $item['link'] }}" />
+                @else
+                    <a href="{{ $item['text'] }}" target="_blank">
+                        [[外站视频：{{ $item['text'] }}]]
+                    </a>
+                @endif
                 @break
-            @case (7) {{--换行 {"type":"7","text":"\n"} not found in many posts --}}
+            @case (7) {{--换行 {"type":"7", "text":"\n"} not found in many posts --}}
                 <br />
                 @break
             @case (9) {{--电话 {"text": "12345678 \d{8}", "type": "9", "phonetype": "2"} --}}

fe/src/components/Post/QueryForm.vue
@@ -200,10 +200,11 @@ export default defineComponent({
             isOrderByInvalid: false,
             isFidInvalid: false
         });
-        const currentQueryType = () => {
+        const getCurrentQueryType = () => {
             const clearedParams = clearedParamsDefaultValue();
             if (_.isEmpty(clearedParams)) { // is there no other params
-                const clearedUniqueParams = clearedUniqueParamsDefaultValue('postTypes', 'orderBy');
+                // ignore the post type param since index query (postID or fid) doesn't restrict them
+                const clearedUniqueParams = _.omit(clearedUniqueParamsDefaultValue(), 'postTypes');
                 if (_.isEmpty(clearedUniqueParams)) { // only fill unique param postTypes and/or orderBy doesn't query anything
                     return 'empty';
                 } else if (clearedUniqueParams.fid !== undefined) {
@@ -263,7 +264,8 @@ export default defineComponent({
             // check query type
             state.isFidInvalid = false;
             const clearedUniqueParams = clearedUniqueParamsDefaultValue();
-            switch (currentQueryType()) {
+            const currentQueryType = getCurrentQueryType();
+            switch (currentQueryType) {
                 case 'empty':
                     notyShow('warning', '请选择贴吧或/并输入查询参数，勿只选择贴子类型参数');
                     return false; // exit early
@@ -273,7 +275,7 @@ export default defineComponent({
                         notyShow('info', '已移除按贴索引查询所不需要的查询贴吧参数');
                         submitRoute(); // update route to match new params without fid
                     }
-                    return true; // index query doesn't restrict on post types
+                    break;
                 case 'search':
                     if (clearedUniqueParams.fid === undefined) {
                         state.isFidInvalid = true; // search query require fid param
@@ -282,6 +284,7 @@ export default defineComponent({
                     break;
                 case 'fid':
             }
+
             const isRequiredPostTypes = (current: PostType[], required?: RequiredPostTypes[string]): required is undefined => {
                 if (required === undefined) return true; // not set means this param accepts any post types
                 required[1] = _.sortBy(required[1]);
@@ -291,20 +294,24 @@ export default defineComponent({
             };
             const requiredPostTypesStringify = (required: NonNullable<RequiredPostTypes[string]>) =>
                 `${required[1].join(required[0] === 'SUB' ? ' | ' : ' & ')}`;
-            // check params required post types
             const postTypes = _.sortBy(useState.uniqueParams.postTypes.value);
+
+            // check params required post types, index query doesn't restrict on post types
             useState.invalidParamsIndex = []; // reset to prevent duplicate indexes
-            useState.params.map(clearParamDefaultValue).forEach((param, paramIndex) => { // we don't filter() here for post types validate
-                if (param === null || param.name === undefined || param.value === undefined) {
-                    useState.invalidParamsIndex.push(paramIndex);
-                } else {
-                    const required = paramsRequiredPostTypes[param.name];
-                    if (!isRequiredPostTypes(postTypes, required)) {
+            if (currentQueryType !== 'postID' || currentQueryType !== 'fid') {
+                useState.params.map(clearParamDefaultValue).forEach((param, paramIndex) => { // we don't filter() here for post types validate
+                    if (param === null || param.name === undefined || param.value === undefined) {
                         useState.invalidParamsIndex.push(paramIndex);
-                        notyShow('warning', `第${paramIndex + 1}个${param.name}参数要求贴子类型为${requiredPostTypesStringify(required)}`);
+                    } else {
+                        const required = paramsRequiredPostTypes[param.name];
+                        if (!isRequiredPostTypes(postTypes, required)) {
+                            useState.invalidParamsIndex.push(paramIndex);
+                            notyShow('warning', `第${paramIndex + 1}个${param.name}参数要求贴子类型为${requiredPostTypesStringify(required)}`);
+                        }
                     }
-                }
-            });
+                });
+            }
+
             // check order by required post types
             state.isOrderByInvalid = false;
             const orderBy = useState.uniqueParams.orderBy.value;
@@ -315,12 +322,15 @@ export default defineComponent({
                     notyShow('warning', `排序方式与查询贴子类型要求不匹配，当前要求贴子类型为${requiredPostTypesStringify(required)}`);
                 }
             }
+
             // return false when there have at least one invalid params
             return _.isEmpty(useState.invalidParamsIndex) && !state.isOrderByInvalid && !state.isFidInvalid;
         };
         const submit = () => {
-            emit('query', flattenParams());
-            if (checkParams()) submitRoute();
+            if (checkParams()) {
+                emit('query', flattenParams());
+                submitRoute();
+            }
         };
         Object.assign(useQueryFormLateBinding, { parseRoute }); // assign() will prevent losing ref
 
@@ -329,9 +339,9 @@ export default defineComponent({
             return `${p.subParam.matchBy === 'explicit' ? '精确' : '正则'}匹配 空格${p.subParam.spaceSplit ? '不' : ''}分割关键词`;
         };
         const currentQueryTypeDesc = computed(() => {
-            if (currentQueryType() === 'fid') return '按吧索引查询';
-            if (currentQueryType() === 'postID') return '按贴索引查询';
-            if (currentQueryType() === 'search') return '搜索查询';
+            if (getCurrentQueryType() === 'fid') return '按吧索引查询';
+            if (getCurrentQueryType() === 'postID') return '按贴索引查询';
+            if (getCurrentQueryType() === 'search') return '搜索查询';
             return '空查询';
         });
 

fe/src/components/Post/useQueryForm.ts
@@ -94,9 +94,9 @@ export default <
     };
     const clearedParamsDefaultValue = (): Array<Partial<TParam>> =>
         _.filter(state.params.map(clearParamDefaultValue)) as Array<Partial<TParam>>; // filter() will remove falsy values like null
-    const clearedUniqueParamsDefaultValue = (...omitParams: string[]): Partial<UniqueParams> =>
+    const clearedUniqueParamsDefaultValue = (): Partial<UniqueParams> =>
         // mapValues() return object which remains keys, pickBy() like filter() for objects
-        _.pickBy(_.mapValues(_.omit(state.uniqueParams, omitParams), clearParamDefaultValue)) as Partial<UniqueParams>;
+        _.pickBy(_.mapValues(state.uniqueParams, clearParamDefaultValue)) as Partial<UniqueParams>;
     const flattenParams = (): ObjUnknown[] => {
         const flattenParam = (param: Partial<Param>) => {
             const flatted: ObjUnknown = {};