0d8f4a3bc2220bacda9eb6f2295dc816aedf5acb
n0099 <n@n0099.net>
2022-01-07 22:37:43 +0800

    - delete all `blade.php` files which had been refactoring in fe @ be/resources/views

--- be/resources/views/module/vue/antd.blade.php
+++
@@ -1,9 +0,0 @@
-@section('style-module')
-    @parent
-    <link href="https://cdn.jsdelivr.net/npm/ant-design-vue@1.6.4/dist/antd.min.css" rel="stylesheet">
-@endsection
-
-@section('script-module')
-    @parent
-    <script src="https://cdn.jsdelivr.net/npm/ant-design-vue@1.6.4/dist/antd-with-locales.min.js"></script>
-@endsection
