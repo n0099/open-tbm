n0099/open-tbm

commit 68c84cda24b93332dbd664cadb3623859a380d4b

* fix possible db exception with duplicated uid by locking latest replier's uid into UserParserAndSaver._savedUsersId @ ExtraSavings() * rename field _latestReplierUsers to _latestRepliers @ ThreadCrawlFacade.cs + method AcquireUidLock() for ThreadCrawlFacade.ExtraSavings() * field _savedUsersId now is readonly and non-nullable @ UserParserAndSaver.cs
* now will return early when param savedThreads is null @ ArchiveCrawlWorker.CrawlReplies()
* replace some occurs of the common lambda param name i with other letter that related to the type of param
@ crawler

crawler/src/Tieba/ClientRequester.cs
@@ -91,7 +91,7 @@ namespace tbm.Crawler
             protoBufFile.Headers.Add("Content-Disposition", "form-data; name=\"data\"; filename=\"file\"");
             var content = new MultipartFormDataContent {protoBufFile};
             // https://stackoverflow.com/questions/30926645/httpcontent-boundary-double-quotes
-            var boundary = content.Headers.ContentType?.Parameters.First(o => o.Name == "boundary");
+            var boundary = content.Headers.ContentType?.Parameters.First(i => i.Value == "boundary");
             if (boundary != null) boundary.Value = boundary.Value?.Replace("\"", "");
 
             var request = new HttpRequestMessage(HttpMethod.Post, url) {Content = content};

crawler/src/Tieba/Crawl/Facade/ThreadCrawlFacade.cs
@@ -2,7 +2,7 @@ namespace tbm.Crawler
 {
     public class ThreadCrawlFacade : BaseCrawlFacade<ThreadPost, ThreadResponse, Thread, ThreadCrawler>
     {
-        private readonly Dictionary<long, TiebaUser> _latestReplierUsers = new();
+        private readonly Dictionary<long, TiebaUser> _latestRepliers = new();
         public delegate ThreadCrawlFacade New(Fid fid, string forumName);
 
         public ThreadCrawlFacade(ILogger<ThreadCrawlFacade> logger, TbmDbContext.New dbContextFactory,
@@ -14,9 +14,14 @@ namespace tbm.Crawler
         { // ExtraSavings() should get invoked after UserParserAndSaver.SaveUsers() by the base.SaveAll()
             // so only users that not exists in parsed users is being inserted
             // note this will bypass user revision detection since its not invoking CommonInSavers.SavePostsOrUsers()
-            var existingUsersId = (from u in db.Users where _latestReplierUsers.Keys.Any(uid => uid == u.Uid) select u.Uid).ToHashSet();
+            var existingUsersId = (from u in db.Users where _latestRepliers.Keys.Any(uid => uid == u.Uid) select u.Uid).ToHashSet();
             existingUsersId.UnionWith(db.ChangeTracker.Entries<TiebaUser>().Select(e => e.Entity.Uid)); // users not exists in db but have already been added and tracking
-            db.AddRange(_latestReplierUsers.ExceptBy(existingUsersId, u => u.Key).Select(p => p.Value));
+            var newLatestRepliers = _latestRepliers.ExceptBy(existingUsersId, i => i.Key).Select(i => i.Value).ToList();
+
+            if (!newLatestRepliers.Any()) return; // quick exits
+            db.AddRange(newLatestRepliers.ExceptBy(
+                Users.AcquireUidLock(newLatestRepliers.Select(u => u.Uid).ToList()),
+                u => u.Uid));
         }
 
         protected void ParseLatestRepliers(ThreadResponse.Types.Data data) =>
@@ -24,7 +29,7 @@ namespace tbm.Crawler
                 Uid = u.Uid,
                 Name = u.Name.NullIfWhiteSpace(),
                 DisplayName = u.Name == u.NameShow ? null : u.NameShow
-            }).ForEach(u => _latestReplierUsers[u.Uid] = u);
+            }).ForEach(u => _latestRepliers[u.Uid] = u);
 
         protected override void PostParseCallback(ThreadResponse response, CrawlRequestFlag flag)
         {
@@ -38,10 +43,10 @@ namespace tbm.Crawler
             if (!users.Any()) return;
             Users.ParseUsers(users);
 
-            ParsedPosts.Values.IntersectBy(data.ThreadList.Select(p => (Tid)p.Tid), p => p.Tid)
-                .Where(t => t.StickyType == null).ForEach(p =>
+            ParsedPosts.Values.IntersectBy(data.ThreadList.Select(t => (Tid)t.Tid), t => t.Tid)
+                .Where(t => t.StickyType == null).ForEach(t =>
                     // fill the values of author manager type from the external user list
-                    p.AuthorManagerType = users.First(u => u.Uid == p.AuthorUid).BawuType.NullIfWhiteSpace());
+                    t.AuthorManagerType = users.First(u => u.Uid == t.AuthorUid).BawuType.NullIfWhiteSpace());
         }
     }
 }

crawler/src/Tieba/Crawl/UserParserAndSaver.cs
@@ -23,7 +23,7 @@ namespace tbm.Crawler
         };
         private static readonly Regex PortraitExtractingRegex = new(@"^(.*?)\?t=(\d+)$", RegexOptions.Compiled, TimeSpan.FromSeconds(1));
         private static readonly HashSet<Uid> UsersIdLock = new();
-        private IEnumerable<Uid>? _savedUsersId;
+        private readonly List<Uid> _savedUsersId = new();
         private readonly ILogger<UserParserAndSaver> _logger;
         private readonly ConcurrentDictionary<Uid, TiebaUser> _users = new();
 
@@ -71,18 +71,18 @@ namespace tbm.Crawler
                     e.Data["raw"] = Helper.UnescapedJsonSerialize(el);
                     throw new("User parse error.", e);
                 }
-            }).OfType<TiebaUser>().ForEach(i => _users[i.Uid] = i);
+            }).OfType<TiebaUser>().ForEach(u => _users[u.Uid] = u);
 
         public void SaveUsers<TPost>(TbmDbContext db, BaseSaver<TPost> postSaver) where TPost : class, IPost
         {
             if (_users.IsEmpty) return;
             lock (UsersIdLock)
             {
-                var usersExceptLocked = _users.ExceptBy(UsersIdLock, u => u.Key).ToDictionary(i => i.Key, i => i.Value);
+                var usersExceptLocked = _users.ExceptBy(UsersIdLock, i => i.Key).ToDictionary(i => i.Key, i => i.Value);
                 if (!usersExceptLocked.Any()) return;
-                _savedUsersId = usersExceptLocked.Keys;
+                _savedUsersId.AddRange(usersExceptLocked.Keys);
                 UsersIdLock.UnionWith(_savedUsersId);
-                var existingUsersByUid = (from user in db.Users where usersExceptLocked.Keys.Contains(user.Uid) select user).ToDictionary(i => i.Uid);
+                var existingUsersByUid = (from user in db.Users where usersExceptLocked.Keys.Contains(user.Uid) select user).ToDictionary(u => u.Uid);
 
                 SavePostsOrUsers(_logger, postSaver.TiebaUserFieldChangeIgnorance, usersExceptLocked, db,
                     u => new UserRevision {Time = u.UpdatedAt, Uid = u.Uid, TriggeredBy = TriggeredByPostSaverMap[postSaver.GetType()]},
@@ -91,9 +91,20 @@ namespace tbm.Crawler
             }
         }
 
+        public IEnumerable<Uid> AcquireUidLock(List<Uid> usersId)
+        {
+            lock (UsersIdLock)
+            {
+                _savedUsersId.AddRange(usersId);
+                var exceptLocked = usersId.Except(UsersIdLock);
+                UsersIdLock.UnionWith(usersId);
+                return exceptLocked;
+            }
+        }
+
         public void PostSaveCallback()
         {
-            if (_savedUsersId != null) lock (UsersIdLock) UsersIdLock.ExceptWith(_savedUsersId);
+            lock (UsersIdLock) if (_savedUsersId.Any()) UsersIdLock.ExceptWith(_savedUsersId);
         }
     }
 }

crawler/src/Tieba/Crawl/ArchiveCrawlWorker.cs
@@ -47,11 +47,11 @@ namespace tbm.Crawler
         {
             await using var scope1 = _scope0.BeginLifetimeScope();
             var crawler = scope1.Resolve<ThreadCrawlFacade.New>()(fid, forumName);
-            var savedThreads = (await crawler.CrawlPageRange(page, page)).SavePosts();
+            var savedThreads = (await crawler.CrawlPageRange(page, page)).SaveAll();
             if (savedThreads != null)
             {
                 await scope1.Resolve<ThreadLateCrawlerAndSaver.New>()(fid)
-                    .Crawl(savedThreads.NewlyAdded.Select(t => new ThreadLateCrawlerAndSaver.TidAndFailedCount(t.Tid, 0)));
+                    .Crawl(savedThreads.NewlyAdded.ToDictionary(t => t.Tid, _ => (FailedCount)0));
             }
             return savedThreads;
         }
@@ -59,24 +59,24 @@ namespace tbm.Crawler
         private async Task<SavedRepliesByTid> CrawlReplies(SaverChangeSet<ThreadPost>? savedThreads, Fid fid)
         {
             var shouldCrawlReplyTid = new HashSet<Tid>();
-            if (savedThreads != null)
-            {
-                savedThreads.NewlyAdded.ForEach(i => shouldCrawlReplyTid.Add(i.Tid));
-                savedThreads.Existing.ForEach(beforeAndAfter =>
-                {
-                    var (before, after) = beforeAndAfter;
-                    if (before.ReplyNum != after.ReplyNum
-                        || before.LatestReplyTime != after.LatestReplyTime
-                        || before.LatestReplierUid != after.LatestReplierUid)
-                        _ = shouldCrawlReplyTid.Add(before.Tid);
-                });
-            }
             var savedRepliesByTid = new SavedRepliesByTid();
+            if (savedThreads == null) return savedRepliesByTid;
+
+            savedThreads.NewlyAdded.ForEach(t => shouldCrawlReplyTid.Add(t.Tid));
+            savedThreads.Existing.ForEach(beforeAndAfter =>
+            {
+                var (before, after) = beforeAndAfter;
+                if (before.ReplyNum != after.ReplyNum
+                    || before.LatestReplyTime != after.LatestReplyTime
+                    || before.LatestReplierUid != after.LatestReplierUid)
+                    _ = shouldCrawlReplyTid.Add(before.Tid);
+            });
+
             await Task.WhenAll(shouldCrawlReplyTid.Select(async tid =>
             {
                 await using var scope1 = _scope0.BeginLifetimeScope();
                 var crawler = scope1.Resolve<ReplyCrawlFacade.New>()(fid, tid);
-                savedRepliesByTid.SetIfNotNull(tid, (await crawler.CrawlPageRange(1)).SavePosts());
+                savedRepliesByTid.SetIfNotNull(tid, (await crawler.CrawlPageRange(1)).SaveAll());
             }));
             return savedRepliesByTid;
         }
@@ -86,9 +86,9 @@ namespace tbm.Crawler
             var shouldCrawlSubReplyPid = savedRepliesByTid.Aggregate(new HashSet<(Tid, Pid)>(), (shouldCrawl, tidAndReplies) =>
             {
                 var (tid, replies) = tidAndReplies;
-                replies.NewlyAdded.ForEach(i =>
+                replies.NewlyAdded.ForEach(r =>
                 {
-                    if (i.SubReplyNum != null) _ = shouldCrawl.Add((tid, i.Pid));
+                    if (r.SubReplyNum != null) _ = shouldCrawl.Add((tid, r.Pid));
                 });
                 replies.Existing.ForEach(beforeAndAfter =>
                 {
@@ -101,7 +101,7 @@ namespace tbm.Crawler
             {
                 var (tid, pid) = tidAndPid;
                 await using var scope1 = _scope0.BeginLifetimeScope();
-                _ = (await scope1.Resolve<SubReplyCrawlFacade.New>()(fid, tid, pid).CrawlPageRange(1)).SavePosts();
+                _ = (await scope1.Resolve<SubReplyCrawlFacade.New>()(fid, tid, pid).CrawlPageRange(1)).SaveAll();
             }));
         }
     }

crawler/src/Tieba/Crawl/MainCrawlWorker.cs
@@ -91,7 +91,8 @@ namespace tbm.Crawler
             await Task.WhenAll(savedThreads.Select(async threads =>
             {
                 await using var scope3 = _scope0.BeginLifetimeScope();
-                await scope3.Resolve<ThreadLateCrawlerAndSaver.New>()(fid).Crawl(threads.NewlyAdded.ToDictionary(i => i.Tid, _ => (FailedCount)0));
+                await scope3.Resolve<ThreadLateCrawlerAndSaver.New>()(fid)
+                    .Crawl(threads.NewlyAdded.ToDictionary(t => t.Tid, _ => (FailedCount)0));
             }));
 
             return savedThreads;
@@ -101,7 +102,7 @@ namespace tbm.Crawler
         {
             var shouldCrawlReplyTid = savedThreads.Aggregate(new HashSet<Tid>(), (shouldCrawl, threads) =>
             {
-                threads.NewlyAdded.ForEach(i => shouldCrawl.Add(i.Tid));
+                threads.NewlyAdded.ForEach(t => shouldCrawl.Add(t.Tid));
                 threads.Existing.ForEach(beforeAndAfter =>
                 {
                     var (before, after) = beforeAndAfter;
@@ -127,9 +128,9 @@ namespace tbm.Crawler
             var shouldCrawlSubReplyPid = savedRepliesByTid.Aggregate(new HashSet<(Tid, Pid)>(), (shouldCrawl, tidAndReplies) =>
             {
                 var (tid, replies) = tidAndReplies;
-                replies.NewlyAdded.ForEach(i =>
+                replies.NewlyAdded.ForEach(r =>
                 {
-                    if (i.SubReplyNum != null) _ = shouldCrawl.Add((tid, i.Pid));
+                    if (r.SubReplyNum != null) _ = shouldCrawl.Add((tid, r.Pid));
                 });
                 replies.Existing.ForEach(beforeAndAfter =>
                 {

crawler/src/Tieba/Crawl/RetryCrawlWorker.cs
@@ -35,7 +35,7 @@ namespace tbm.Crawler
                         await using var scope1 = _scope0.BeginLifetimeScope();
                         var db = scope1.Resolve<TbmDbContext.New>()(0);
                         var tidAndFidRecords = from t in db.PostsIndex where t.Type == "thread" && failed.Keys.Contains(t.Tid) select new {t.Fid, t.Tid};
-                        foreach (var tidGroupByFid in tidAndFidRecords.ToList().GroupBy(record => record.Fid))
+                        foreach (var tidGroupByFid in tidAndFidRecords.ToList().GroupBy(i => i.Fid))
                         {
                             FailedCount FailedCountSelector(Tid tid) => failed[tid].First().Value; // it should always contains only one page which is 1
                             var tidAndFailedCountList = tidGroupByFid.ToDictionary(g => g.Tid, g => FailedCountSelector(g.Tid));

crawler/src/Tieba/Crawl/CrawlerLocks.cs
@@ -27,9 +27,9 @@ namespace tbm.Crawler
             lock (_failed)
             {
                 _logger.LogTrace("Lock: type={} crawlingIdsCount={} crawlingPagesCount={} crawlingPagesCountKeyById={} failedIdsCount={} failedPagesCount={} failedAll={}", PostType,
-                    _crawling.Count, _crawling.Values.Select(i => i.Count).Sum(),
+                    _crawling.Count, _crawling.Values.Select(d => d.Count).Sum(),
                     Helper.UnescapedJsonSerialize(_crawling.ToDictionary(i => i.Key, i => i.Value.Count)),
-                    _failed.Count, _failed.Values.Select(i => i.Count).Sum(), Helper.UnescapedJsonSerialize(_failed));
+                    _failed.Count, _failed.Values.Select(d => d.Count).Sum(), Helper.UnescapedJsonSerialize(_failed));
             }
         }
 
@@ -41,7 +41,7 @@ namespace tbm.Crawler
                 var now = (Time)DateTimeOffset.Now.ToUnixTimeSeconds();
                 if (!_crawling.ContainsKey(index))
                 { // if no one is locking any page in index, just insert pages then return it as is
-                    var pageTimeDict = lockFreePages.Select(p => KeyValuePair.Create(p, now));
+                    var pageTimeDict = lockFreePages.Select(i => KeyValuePair.Create(i, now));
                     var newPage = new ConcurrentDictionary<Page, Time>(pageTimeDict);
                     if (_crawling.TryAdd(index, newPage)) return lockFreePages;
                 }
@@ -74,7 +74,7 @@ namespace tbm.Crawler
                 }
                 lock (pagesLock)
                 {
-                    pages.ForEach(p => pagesLock.TryRemove(p, out _));
+                    pages.ForEach(i => pagesLock.TryRemove(i, out _));
                     if (pagesLock.IsEmpty) _crawling.TryRemove(index, out _);
                 }
             }
@@ -107,9 +107,9 @@ namespace tbm.Crawler
         {
             lock (_failed)
             {
-                var copyOfFailed = _failed.ToDictionary(p => p.Key, p =>
+                var copyOfFailed = _failed.ToDictionary(i => i.Key, i =>
                 {
-                    lock (p.Value) return new Dictionary<Page, FailedCount>(p.Value);
+                    lock (i.Value) return new Dictionary<Page, FailedCount>(i.Value);
                 });
                 _failed.Clear();
                 return copyOfFailed;

crawler/src/Tieba/Crawl/Facade/ReplyCrawlFacade.cs
@@ -27,7 +27,7 @@ namespace tbm.Crawler
                 var parentThreadTitle = (from t in db.Threads where t.Tid == _tid select t.Title).FirstOrDefault();
                 if (parentThreadTitle == "")
                 { // thread title will be empty string as a fallback when the thread author haven't write title for this thread
-                    var newTitle = data.PostList.FirstOrDefault(p => p.Floor == 1)?.Title;
+                    var newTitle = data.PostList.FirstOrDefault(r => r.Floor == 1)?.Title;
                     if (newTitle != null)
                     {
                         db.Attach(new ThreadPost {Tid = _tid, Title = newTitle}).Property(t => t.Title).IsModified = true;
@@ -42,12 +42,12 @@ namespace tbm.Crawler
             if (!users.Any()) return;
             Users.ParseUsers(users);
 
-            ParsedPosts.Values.IntersectBy(data.PostList.Select(p => p.Pid), p => p.Pid).ForEach(p =>
+            ParsedPosts.Values.IntersectBy(data.PostList.Select(r => r.Pid), r => r.Pid).ForEach(r =>
             { // fill the values for some field of reply from user list which is out of post list
-                var author = users.First(u => u.Uid == p.AuthorUid);
-                p.AuthorManagerType = author.BawuType.NullIfWhiteSpace(); // will be null if he's not a moderator
-                p.AuthorExpGrade = (ushort)author.LevelId; // will be null when author is a historical anonymous user
-                p.Tid = _tid;
+                var author = users.First(u => u.Uid == r.AuthorUid);
+                r.AuthorManagerType = author.BawuType.NullIfWhiteSpace(); // will be null if he's not a moderator
+                r.AuthorExpGrade = (ushort)author.LevelId; // will be null when author is a historical anonymous user
+                r.Tid = _tid;
             });
         }
     }

commit c52ceb802c2d661efa53f9df1d72568a7c4e6a65

* fix a typo that introduced in 68c84cda24b93332dbd664cadb3623859a380d4b @ ClientRequester.PostProtoBuf()
* fix missing up intersect set with difference set @ ThreadCrawlFacade.ExtraSavings()
* now will only add id that not being locked into _savedUsersId @ UserParserAndSaver.AcquireUidLock()

* now will try to crawl all (sub) replies under thread instead of only updated ones
@ ArchiveCrawlWorker.cs
$ mv Tieba/Crawl/*CrawlWorker.cs Tieba/Crawl/Worker
@ crawler

crawler/src/Tieba/ClientRequester.cs
@@ -91,7 +91,7 @@ namespace tbm.Crawler
             protoBufFile.Headers.Add("Content-Disposition", "form-data; name=\"data\"; filename=\"file\"");
             var content = new MultipartFormDataContent {protoBufFile};
             // https://stackoverflow.com/questions/30926645/httpcontent-boundary-double-quotes
-            var boundary = content.Headers.ContentType?.Parameters.First(i => i.Value == "boundary");
+            var boundary = content.Headers.ContentType?.Parameters.First(i => i.Name == "boundary");
             if (boundary != null) boundary.Value = boundary.Value?.Replace("\"", "");
 
             var request = new HttpRequestMessage(HttpMethod.Post, url) {Content = content};

crawler/src/Tieba/Crawl/Facade/ThreadCrawlFacade.cs
@@ -18,9 +18,9 @@ namespace tbm.Crawler
             existingUsersId.UnionWith(db.ChangeTracker.Entries<TiebaUser>().Select(e => e.Entity.Uid)); // users not exists in db but have already been added and tracking
             var newLatestRepliers = _latestRepliers.ExceptBy(existingUsersId, i => i.Key).Select(i => i.Value).ToList();
 
-            if (!newLatestRepliers.Any()) return; // quick exits
-            db.AddRange(newLatestRepliers.ExceptBy(
-                Users.AcquireUidLock(newLatestRepliers.Select(u => u.Uid).ToList()),
+            if (!newLatestRepliers.Any()) return;
+            db.AddRange(newLatestRepliers.IntersectBy(
+                Users.AcquireUidLock(newLatestRepliers.Select(u => u.Uid)),
                 u => u.Uid));
         }
 

crawler/src/Tieba/Crawl/UserParserAndSaver.cs
@@ -91,13 +91,14 @@ namespace tbm.Crawler
             }
         }
 
-        public IEnumerable<Uid> AcquireUidLock(List<Uid> usersId)
+        public IEnumerable<Uid> AcquireUidLock(IEnumerable<long> usersId)
         {
             lock (UsersIdLock)
             {
-                _savedUsersId.AddRange(usersId);
-                var exceptLocked = usersId.Except(UsersIdLock);
-                UsersIdLock.UnionWith(usersId);
+                var exceptLocked = usersId.Except(UsersIdLock).ToList();
+                if (!exceptLocked.Any()) return exceptLocked;
+                _savedUsersId.AddRange(exceptLocked);
+                UsersIdLock.UnionWith(exceptLocked);
                 return exceptLocked;
             }
         }

crawler/src/Tieba/Crawl/Worker/ArchiveCrawlWorker.cs
@@ -61,16 +61,7 @@ namespace tbm.Crawler
             var shouldCrawlReplyTid = new HashSet<Tid>();
             var savedRepliesByTid = new SavedRepliesByTid();
             if (savedThreads == null) return savedRepliesByTid;
-
-            savedThreads.NewlyAdded.ForEach(t => shouldCrawlReplyTid.Add(t.Tid));
-            savedThreads.Existing.ForEach(beforeAndAfter =>
-            {
-                var (before, after) = beforeAndAfter;
-                if (before.ReplyNum != after.ReplyNum
-                    || before.LatestReplyTime != after.LatestReplyTime
-                    || before.LatestReplierUid != after.LatestReplierUid)
-                    _ = shouldCrawlReplyTid.Add(before.Tid);
-            });
+            savedThreads.AllAfter.ForEach(t => shouldCrawlReplyTid.Add(t.Tid));
 
             await Task.WhenAll(shouldCrawlReplyTid.Select(async tid =>
             {
@@ -86,15 +77,7 @@ namespace tbm.Crawler
             var shouldCrawlSubReplyPid = savedRepliesByTid.Aggregate(new HashSet<(Tid, Pid)>(), (shouldCrawl, tidAndReplies) =>
             {
                 var (tid, replies) = tidAndReplies;
-                replies.NewlyAdded.ForEach(r =>
-                {
-                    if (r.SubReplyNum != null) _ = shouldCrawl.Add((tid, r.Pid));
-                });
-                replies.Existing.ForEach(beforeAndAfter =>
-                {
-                    var (before, after) = beforeAndAfter;
-                    if (after.SubReplyNum != null && before.SubReplyNum != after.SubReplyNum) _ = shouldCrawl.Add((tid, before.Pid));
-                });
+                replies.AllAfter.ForEach(r => shouldCrawl.Add((tid, r.Pid)));
                 return shouldCrawl;
             });
             await Task.WhenAll(shouldCrawlSubReplyPid.Select(async tidAndPid =>