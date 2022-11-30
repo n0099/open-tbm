9d8aada4f1cd67f2262667810f69908eb6243de3
n0099 <n@n0099.net>
2022-12-01 01:48:21 +0800

    * fix exception `NSonic.AssertionException: Expected OK response : ERR invalid_format` which caused by not escaping `\` * cut the pushing text into chunks with a limit of max 10k bytes to prevent the `NSonic.AssertionException: Message was too long` * flip the bucket name to the prefix of collection name, also affects appsettings.json @ PushPost() * turn private field _ingest and _bucket into public get-only prop @ InsertAllPostContentsIntoSonicWorker.SonicPusher
    * now will flush the collection in bucket which is pending to be pushed, and `TRIGGER consolidate` under control channel after all is finished @ ExecuteAsync()
    @ InsertAllPostContentsIntoSonicWorker.cs
    @ crawler

--- crawler/src/Tieba/Crawl/Worker/InsertAllPostContentsIntoSonicWorker.cs
+++ crawler/src/Tieba/Crawl/Worker/InsertAllPostContentsIntoSonicWorker.cs
@@ -7,20 +7,20 @@ namespace tbm.Crawler
     {
         public class SonicPusher
         {
+            public ISonicIngestConnection Ingest { get; }
+            public string CollectionPrefix { get; }
             private readonly ILogger<SonicPusher> _logger;
-            private readonly ISonicIngestConnection _ingest;
-            private readonly string _bucket;
 
             public SonicPusher(ILogger<SonicPusher> logger, IConfiguration config)
             {
                 _logger = logger;
                 config = config.GetSection("Sonic");
-                _ingest = NSonicFactory.Ingest(
+                Ingest = NSonicFactory.Ingest(
                     config.GetValue("Hostname", "localhost"),
                     config.GetValue("Port", 1491),
                     config.GetValue("Secret", "SecretPassword")
                 );
-                _bucket = config.GetValue("Bucket", "tbm") ?? "tbm";
+                CollectionPrefix = config.GetValue<string>("CollectionPrefix") ?? "tbm_";
             }
 
             public void PushPost(Fid fid, string postType, PostId postId, byte[]? postContent)
@@ -28,25 +28,29 @@ namespace tbm.Crawler
                 if (postContent == null) return;
                 var content = PostContentWrapper.Parser.ParseFrom(postContent).Value.Aggregate("", (acc, content) => acc + content.Text).Trim();
                 if (content == "") return;
-                content = content.Replace("\n", "\\n").Replace("\"", "\\\""); // https://github.com/spikensbror-dotnet/nsonic/pull/10
+                content = content.Replace("\\", "\\\\").Replace("\n", "\\n").Replace("\"", "\\\""); // https://github.com/spikensbror-dotnet/nsonic/pull/10
                 try
                 {
-                    _ingest.Push($"f{fid}_{postType}", _bucket, postId.ToString(), content, "cmn");
+                    foreach (var text in content.Chunk(10000)) // https://github.com/spikensbror-dotnet/nsonic/issues/11
+                        Ingest.Push($"{CollectionPrefix}f{fid}", postType, postId.ToString(), text.ToString(), "cmn");
                 }
                 catch (Exception e)
                 {
-                    _logger.LogError(e, "Error while pushing the content of {} for {} in fid {} into sonic, content={}", postId, postType, fid, content);
+                    _logger.LogError(e, "Error while pushing the content of post id {} for {} in fid {} into sonic, content={}", postId, postType, fid, content);
                 }
             }
         }
 
         private readonly ILogger<InsertAllPostContentsIntoSonicWorker> _logger;
+        private readonly IConfiguration _config;
         private readonly ILifetimeScope _scope0;
         private readonly SonicPusher _pusher;
 
-        public InsertAllPostContentsIntoSonicWorker(ILogger<InsertAllPostContentsIntoSonicWorker> logger, ILifetimeScope scope0, SonicPusher pusher)
+        public InsertAllPostContentsIntoSonicWorker(ILogger<InsertAllPostContentsIntoSonicWorker> logger,
+            IConfiguration config, ILifetimeScope scope0, SonicPusher pusher)
         {
             _logger = logger;
+            _config = config.GetSection("Sonic");
             _scope0 = scope0;
             _pusher = pusher;
         }
@@ -57,16 +61,25 @@ namespace tbm.Crawler
             foreach (var fid in from f in scope1.Resolve<TbmDbContext.New>()(0).ForumsInfo select f.Fid)
             {
                 var dbWithFid = scope1.Resolve<TbmDbContext.New>()(fid);
-                PushPostContents(fid, "replies", dbWithFid.ReplyContents, r => _pusher.PushPost(fid, "replies", r.Pid, r.Content));
-                PushPostContents(fid, "sub replies", dbWithFid.SubReplyContents, sr => _pusher.PushPost(fid, "replies", sr.Spid, sr.Content));
+                _pusher.Ingest.FlushBucket($"{_pusher.CollectionPrefix}f{fid}", "replies");
+                PushPostContents(fid, "replies", dbWithFid.ReplyContents.AsNoTracking(),
+                    r => _pusher.PushPost(fid, "replies", r.Pid, r.Content));
+                _pusher.Ingest.FlushBucket($"{_pusher.CollectionPrefix}f{fid}", "subReplies");
+                PushPostContents(fid, "sub replies", dbWithFid.SubReplyContents.AsNoTracking(),
+                    sr => _pusher.PushPost(fid, "subReplies", sr.Spid, sr.Content));
             }
+            NSonicFactory.Control(
+                _config.GetValue("Hostname", "localhost"),
+                _config.GetValue("Port", 1491),
+                _config.GetValue("Secret", "SecretPassword")
+            ).Trigger("consolidate");
         }
 
         private void PushPostContents<T>(uint fid, string postTypeInLog, IEnumerable<T> postContents, Action<T> pushCallback)
         {
             var stopWatch = new Stopwatch();
             var stopWatchPushing = new Stopwatch();
-            _logger.LogInformation("Pushing all historical {} content for fid {} started", postTypeInLog, fid);
+            _logger.LogInformation("Pushing all historical {}' content into sonic for fid {} started", postTypeInLog, fid);
             stopWatch.Start();
             var pushedStats = postContents.Aggregate((Count: 0, AvgTime: 0f), (acc, post) =>
             {
@@ -75,7 +88,7 @@ namespace tbm.Crawler
                 return (acc.Count + 1, acc.AvgTime + (1f / stopWatchPushing.ElapsedMilliseconds)); // harmonic mean
             });
             pushedStats.AvgTime /= pushedStats.Count;
-            _logger.LogInformation("Pushing {} historical {} content for fid {} finished after {} ({}ms)",
+            _logger.LogInformation("Pushing {} historical {}' content into sonic for fid {} finished after {} ({}ms)",
                 pushedStats.Count, postTypeInLog, fid, stopWatch.Elapsed.Humanize(), stopWatch.ElapsedMilliseconds);
         }
     }
