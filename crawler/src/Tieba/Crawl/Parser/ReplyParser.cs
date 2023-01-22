c414ca3429ceb1cd4a7607c10fb79cb608b7cd2d
n0099 <n@n0099.net>
2023-01-23 06:14:20 +0800

    + static field `ValidateContentImageFilenameRegex` to test again two variant lengths of filename within the tieba image URL * now will also reset field `Content.Src` to the protoBuf default value @ `Convert()` @ ReplyParser.cs * fix possible duplicate entities being tracked when there are multiple identify images within a single reply @ `ReplySaver.SavePosts()`
    - entity class `TiebaUser` no longer inherits `IEquatable<>`, so move the implemented method `Equals()` to `CommonInSavers.IsSameUser()`
    + method `IsLatestReplierUser()` that is extracted from `SavePostsOrUsers()` @ CommonInSavers.cs
    @ crawler

--- crawler/src/Tieba/Crawl/Parser/ReplyParser.cs
+++ crawler/src/Tieba/Crawl/Parser/ReplyParser.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace tbm.Crawler.Tieba.Crawl.Parser;
 
 public class ReplyParser : BaseParser<ReplyPost, Reply>
@@ -7,6 +9,8 @@ public class ReplyParser : BaseParser<ReplyPost, Reply>
     public ReplyParser(ILogger<ReplyParser> logger) => _logger = logger;
 
     protected override PostId PostIdSelector(ReplyPost post) => post.Pid;
+    public static readonly Regex ValidateContentImageFilenameRegex = // length with 24 char is only appeared in legacy replies
+        new("^(?:[0-9a-f]{40}|[0-9a-f]{24})$", RegexOptions.Compiled);
 
     protected override IEnumerable<ReplyPost> ParsePostsInternal(IList<Reply> inPosts, List<User?> outUsers) => inPosts.Select(Convert);
 
@@ -20,14 +24,17 @@ public class ReplyParser : BaseParser<ReplyPost, Reply>
             inPost.Content.Where(c => c.Type == 3).ForEach(c =>
             { // reset with the protoBuf default values to remove these image related fields that have similar values
                 if (!Uri.TryCreate(c.OriginSrc, UriKind.Absolute, out var uri)) return;
-                c.BigCdnSrc = "";
+                c.Src = "";
                 c.CdnSrc = "";
                 c.CdnSrcActive = "";
+                c.BigCdnSrc = "";
                 c.ShowOriginalBtn = 0;
                 c.IsLongPic = 0;
+                var urlFilename = Path.GetFileNameWithoutExtension(uri.AbsolutePath);
                 // only remains the image unique identity at the end of url as "filename", drops domain, path and file extension from url
-                if (uri.Host is "tiebapic.baidu.com" or "imgsrc.baidu.com")
-                    c.OriginSrc = Path.GetFileNameWithoutExtension(uri.AbsolutePath);
+                if (uri.Host is "tiebapic.baidu.com" or "imgsrc.baidu.com" or "hiphotos.baidu.com" // http://hiphotos.baidu.com/bhitozratlo/pic/item/f1671ef3678e7608352accad.jpg
+                    && ValidateContentImageFilenameRegex.IsMatch(urlFilename))
+                    c.OriginSrc = urlFilename;
                 else if (uri.Host is not "tb2.bdstatic.com") // http://tb2.bdstatic.com/tb/cms/commonsub/editor/images/qw_cat_small/qw_cat_0001.gif
                     _logger.LogInformation("Detected an image in the content of reply with pid {} references to {}"
                                            + " instead of common domains of tieba image hosting service, content={}",
