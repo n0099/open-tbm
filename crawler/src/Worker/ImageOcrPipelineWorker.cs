caf582abef4fb25edf623ba5652aadffed8bd41f
n0099 <n@n0099.net>
2023-02-14 03:20:58 +0800

    * now will rerun PaddleOCR text detection for cropped text boxes, which have non-zero rotation degrees, from the original image @ `ExecuteAsync()` + method `ProcessTextBoxes` and their returned types as records
    + record `PaddleOcrDetectionResult` and `TextBoxAndDegrees` as returned types of method `RequestPaddleOcrForDetection()`
    * rename param `imagesKeyByUrlFilename` to `imagesKeyById` @ `RequestPaddleOcrForDetection()`
    
    * rename method `FindRotationAngle()` to `GetRotationDegrees()` @ TextBox
    + nested class ResultsConverter to handle the value of `Results` in json might be an empty string
    @ nested class `PaddleOcrDetectionResponse`
    @ ImageOcrPipelineWorker.cs
    @ crawler

--- crawler/src/Worker/ImageOcrPipelineWorker.cs
+++ crawler/src/Worker/ImageOcrPipelineWorker.cs
@@ -27,40 +27,92 @@ public class ImageOcrPipelineWorker : BackgroundService
                 imagesUrlFilename.Select(async filename =>
                     (filename, bytes: await _http.GetByteArrayAsync(filename + ".jpg", stoppingToken)))))
             .ToDictionary(t => t.filename, t => t.bytes);
-        var textBoxesKeyByUrlFilename = await RequestPaddleOcrForDetection(imagesKeyByUrlFilename, stoppingToken);
-        imagesKeyByUrlFilename
-            .Join(textBoxesKeyByUrlFilename, pair => pair.Key, t => t.UrlFilename,
-                (pair, tuple) => (UrlFilename: pair.Key, ImageBytes: pair.Value, tuple.BoxesAndAngles))
-            .ForEach(t =>
+        Task<ImageAndProcessedTextBoxes> ProcessTextBoxesWithStoppingToken(PaddleOcrDetectionResult i) => ProcessTextBoxes(i, stoppingToken);
+        var processedTextBoxes = await Task.WhenAll(
+            (await RequestPaddleOcrForDetection(imagesKeyByUrlFilename, stoppingToken))
+            .Select(ProcessTextBoxesWithStoppingToken));
+
+        var redetectedTextBoxesWithRotationDegrees = await Task.WhenAll(
+            processedTextBoxes.Select(i =>
             {
-                using var img = Image.Load(t.ImageBytes, out var format);
-                t.BoxesAndAngles.ForEach(t2 =>
-                {
-                    if (t2.Angle == 0) return;
-                    img.Clone(i => i.Crop(t2.TextBox.ToCircumscribedRectangle()).Rotate(t2.Angle))
-                        .SaveAsPng($@"{t2.Angle}{t2.TextBox.ToCircumscribedRectangle()}.png");
-                });
-            });
+                var textBoxesKeyByIdAndBoundary = i.ProcessedTextBoxes
+                    .Where(b => b.RotationDegrees != 0)
+                    .ToDictionary(b => $"{i.ImageId} {b.TextBoxBoundary}", b => b.ProcessedImageBytes);
+                return RequestPaddleOcrForDetection(textBoxesKeyByIdAndBoundary, stoppingToken);
+            }));
+        foreach (var i in redetectedTextBoxesWithRotationDegrees)
+        {
+            var d = await Task.WhenAll(i.Select(ProcessTextBoxesWithStoppingToken));
+            d.ForEach(d2 => d2.ProcessedTextBoxes.ForEach(b =>
+                File.WriteAllBytes($"{b.TextBoxBoundary}.png", b.ProcessedImageBytes)));
+        }
     }
 
-    private async Task<IEnumerable<(string UrlFilename, IEnumerable<(PaddleOcrDetectionResponse.TextBox TextBox, float Angle)> BoxesAndAngles)>>
-        RequestPaddleOcrForDetection(Dictionary<string, byte[]> imagesKeyByUrlFilename, CancellationToken stoppingToken)
+    private record ProcessedTextBox(string TextBoxBoundary, byte[] ProcessedImageBytes, float RotationDegrees);
+
+    private record ImageAndProcessedTextBoxes(string ImageId, IEnumerable<ProcessedTextBox> ProcessedTextBoxes);
+
+    private static async Task<ImageAndProcessedTextBoxes>
+        ProcessTextBoxes(PaddleOcrDetectionResult detectionResult, CancellationToken stoppingToken = default)
     {
-        var requestPayload = new PaddleOcrRequestPayload(imagesKeyByUrlFilename.Values.Select(Convert.ToBase64String));
+        using var image = Image.Load(detectionResult.ImageBytes);
+        return new(detectionResult.ImageId, await Task.WhenAll(detectionResult.TextBoxes
+            .Where(textBoxAndDegrees => textBoxAndDegrees.RotationDegrees != 0)
+            .Select(async textBoxAndDegrees =>
+            {
+                var (textBox, degrees) = textBoxAndDegrees;
+                var circumscribed = textBox.ToCircumscribedRectangle();
+                var processedImageStream = new MemoryStream();
+                await image.Clone(context =>
+                    {
+                        var cropped = context.Crop(circumscribed);
+                        if (degrees != 0) _ = cropped.Rotate(degrees);
+                    })
+                    .SaveAsPngAsync(processedImageStream, stoppingToken);
+                var rectangleBoundary = $"{circumscribed.Width}x{circumscribed.Height}@{circumscribed.X},{circumscribed.Y}";
+                return new ProcessedTextBox(rectangleBoundary, processedImageStream.ToArray(), degrees);
+            })));
+    }
+
+    private record TextBoxAndDegrees(PaddleOcrDetectionResponse.TextBox TextBox, float RotationDegrees);
+
+    private record PaddleOcrDetectionResult(string ImageId, byte[] ImageBytes, IEnumerable<TextBoxAndDegrees> TextBoxes);
+
+    private async Task<IEnumerable<PaddleOcrDetectionResult>>
+        RequestPaddleOcrForDetection(Dictionary<string, byte[]> imagesKeyById, CancellationToken stoppingToken = default)
+    {
+        var requestPayload = new PaddleOcrRequestPayload(imagesKeyById.Values.Select(Convert.ToBase64String));
         var response = await _http.PostAsJsonAsync(_paddleOcrDetectionEndpoint, requestPayload, stoppingToken);
         var detectionResponse = await response.Content.ReadFromJsonAsync<PaddleOcrDetectionResponse>
             (PaddleOcrDetectionResponse.JsonSerializerOptions, stoppingToken);
-        if (detectionResponse is not {Msg: ""} || detectionResponse.Status != "000")
+        if (!response.IsSuccessStatusCode
+            || detectionResponse?.Results == null
+            || detectionResponse.Msg != ""
+            || detectionResponse.Status != "000")
             throw new("PaddleOcrEndpoint/predict/ocr_det responded with non zero status or empty msg"
                       + $"raw={await response.Content.ReadAsStringAsync(stoppingToken)}");
-        return imagesKeyByUrlFilename.Keys.Zip(detectionResponse.Results)
-            .Select(t => (t.First, t.Second.Select(i => (i.TextBox, i.TextBox.FindRotationAngle()))));
+        return imagesKeyById.Zip(detectionResponse.Results)
+            .Select(t => new PaddleOcrDetectionResult(t.First.Key, t.First.Value,
+                t.Second.Select(i => new TextBoxAndDegrees(i.TextBox, i.TextBox.GetRotationDegrees()))));
     }
 
     private record PaddleOcrRequestPayload(IEnumerable<string> Images);
 
     private record PaddleOcrDetectionResponse(string Msg, PaddleOcrDetectionResponse.Result[][] Results, string Status)
     {
+        private class ResultsConverter : JsonConverter<Result[][]>
+        {
+            public override Result[][]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+                reader.TokenType == JsonTokenType.String && reader.GetString() == ""
+                    ? null
+                    : JsonSerializer.Deserialize<Result[][]>(ref reader, options);
+
+            public override void Write(Utf8JsonWriter writer, Result[][] value, JsonSerializerOptions options) => throw new NotImplementedException();
+        }
+
+        [JsonConverter(typeof(ResultsConverter))]
+        public Result[][]? Results { get; } = Results;
+
         public static readonly JsonSerializerOptions JsonSerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
         public record Result(TextBox TextBox)
@@ -72,14 +124,14 @@ public class ImageOcrPipelineWorker : BackgroundService
         [JsonConverter(typeof(TextBoxJsonConverter))]
         public record TextBox(Coordinate TopLeft, Coordinate TopRight, Coordinate BottomLeft, Coordinate BottomRight)
         {
-            // https://www.mathopenref.com/coordbounds.html
             public Rectangle ToCircumscribedRectangle() => Rectangle.FromLTRB(
+                // https://www.mathopenref.com/coordbounds.html
                 Math.Min(TopLeft.X, BottomLeft.X),
-                Math.Min(TopLeft.Y, TopRight.Y), // we are in left-handed cartesian coordinate system so using Min for the topmost point on Y axis
+                Math.Min(TopLeft.Y, TopRight.Y), // in left-handed cartesian coordinate system the minimum point is the topmost point on Y axis
                 Math.Max(TopRight.X, BottomRight.X),
                 Math.Max(BottomLeft.Y, BottomRight.Y));
 
-            public float FindRotationAngle()
+            public float GetRotationDegrees()
             {
                 if (TopLeft.X == BottomLeft.X
                     && TopRight.X == BottomRight.X
@@ -87,7 +139,9 @@ public class ImageOcrPipelineWorker : BackgroundService
                     && BottomLeft.Y == BottomRight.Y) return 0;
                 var xAxisDiff = BottomLeft.X - TopLeft.X;
                 var yAxisDiff = BottomLeft.Y - TopLeft.Y;
-                return (float)Math.Atan2(xAxisDiff, yAxisDiff) * (180 / (float)Math.PI);
+                // https://stackoverflow.com/questions/13002979/how-to-calculate-rotation-angle-from-rectangle-points
+                // https://www.calculator.net/triangle-calculator.html?vc=&vx=4&vy=&va=90&vz=1&vb=&angleunits=d&x=53&y=29
+                return (float)Math.Atan2(xAxisDiff, yAxisDiff) * (float)(180 / Math.PI); // radians to degrees
             }
         }
 
@@ -98,8 +152,8 @@ public class ImageOcrPipelineWorker : BackgroundService
             private static Coordinate ReadCoordinate(ref Utf8JsonReader reader)
             {
                 if (reader.TokenType != JsonTokenType.StartArray) throw new JsonException();
-                int x = 0;
-                int y = 0;
+                var x = 0;
+                var y = 0;
                 var i = 0;
                 while (reader.Read())
                 {
