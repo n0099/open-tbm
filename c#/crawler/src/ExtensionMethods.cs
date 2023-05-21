n0099/open-tbm

commit 9694b66200462c5377cf85aef687f21cfab33d71

- useless null guard since EF Core will throw `System.InvalidOperationException: Cannot create a DbSet for '...' because this type is not included in the model for the context.` @ `BaseSaver.SavePosts()` * using `IEnumerable<>.EquiZip()` from `SuperLinq` to replace length check @ `SaverChangeSet.ctor()` * using checks from `CommunityToolkit.Diagnostics` @ `ExtensionMethods.NextLong()` and `GetInnerExceptions()` * using exception throw helpers from `CommunityToolkit.Diagnostics` @ `BaseCrawlFacade.CrawlPageRange()` and `RetryThenSave()` @ crawler
* using `Guard.IsNotNull` from `CommunityToolkit.Diagnostics` to replace null guard @ `PaddleOcrRecognizerAndDetector.(Detect|Recognize)ImageMatrices()`
* using `IEnumerable<>.EquiZip()` from `SuperLinq` @ `TesseractRecognizer.RecognizePreprocessedTextBox()`
@ imagePipeline

+ NuGet package `CommunityToolkit.Diagnostics` @ shared
@ c#

c#/crawler/src/ExtensionMethods.cs
@@ -11,8 +11,7 @@ public static class ExtensionMethods
     /// <see>https://stackoverflow.com/questions/6651554/random-number-in-long-range-is-this-the-way/13095144#13095144</see>
     public static long NextLong(this Random random, long min, long max)
     {
-        if (max <= min)
-            throw new ArgumentOutOfRangeException(nameof(max), "Max must be > min!");
+        Guard.IsLessThanOrEqualTo(max, min);
 
         var uRange = (ulong)(max - min);
         ulong ulongRand;
@@ -69,7 +68,7 @@ public static class ExtensionMethods
     /// <see>https://stackoverflow.com/questions/9314172/getting-all-messages-from-innerexceptions/9314368#9314368</see>
     public static IEnumerable<Exception> GetInnerExceptions(this Exception ex)
     {
-        if (ex == null) throw new ArgumentNullException(nameof(ex));
+        Guard.IsNotNull(ex);
 
         var inner = ex;
         do

c#/crawler/src/GlobalUsings.cs
@@ -9,6 +9,7 @@ global using Timer = System.Timers.Timer;
 
 global using Autofac;
 global using Autofac.Features.Indexed;
+global using CommunityToolkit.Diagnostics;
 global using Google.Protobuf;
 global using Google.Protobuf.Collections;
 global using Microsoft.EntityFrameworkCore;

c#/crawler/src/Tieba/Crawl/Facade/BaseCrawlFacade.cs
@@ -75,7 +75,7 @@ public abstract class BaseCrawlFacade<TPost, TBaseRevision, TResponse, TPostProt
     public async Task<BaseCrawlFacade<TPost, TBaseRevision, TResponse, TPostProtoBuf, TCrawler>>
         CrawlPageRange(Page startPage, Page endPage = Page.MaxValue, CancellationToken stoppingToken = default)
     { // cancel when startPage is already locked
-        if (_lockingPages.Any()) throw new InvalidOperationException(
+        if (_lockingPages.Any()) ThrowHelper.ThrowInvalidOperationException(
             "CrawlPageRange() can only be called once, a instance of BaseCrawlFacade shouldn't be reuse for other crawls.");
         var acquiredLocks = _locks.AcquireRange(_lockId, new[] {startPage});
         if (!acquiredLocks.Any()) _logger.LogInformation(
@@ -131,7 +131,7 @@ public abstract class BaseCrawlFacade<TPost, TBaseRevision, TResponse, TPostProt
     public async Task<SaverChangeSet<TPost>?> RetryThenSave
         (IList<Page> pages, Func<Page, FailureCount> failureCountSelector, CancellationToken stoppingToken = default)
     {
-        if (_lockingPages.Any()) throw new InvalidOperationException(
+        if (_lockingPages.Any()) ThrowHelper.ThrowInvalidOperationException(
             "RetryPages() can only be called once, a instance of BaseCrawlFacade shouldn't be reuse for other crawls.");
         await CrawlPages(pages, failureCountSelector, stoppingToken);
         return SaveCrawled(stoppingToken);

c#/crawler/src/Tieba/Crawl/Facade/SubReplyCrawlFacade.cs
@@ -16,9 +16,8 @@ public class SubReplyCrawlFacade : BaseCrawlFacade<SubReplyPost, BaseSubReplyRev
         requesterTcs, (locks["subReply"], new(fid, tid, pid)), fid) =>
         (_pusher, _tid, _pid) = (pusher, tid, pid);
 
-    protected override void ThrowIfEmptyUsersEmbedInPosts() =>
-        throw new TiebaException(
-            $"User list in the response of sub reply request for fid {Fid}, tid {_tid}, pid {_pid} is empty.");
+    protected override void ThrowIfEmptyUsersEmbedInPosts() => throw new TiebaException(
+        $"User list in the response of sub reply request for fid {Fid}, tid {_tid}, pid {_pid} is empty.");
 
     protected override void PostParseHook(SubReplyResponse response, CrawlRequestFlag flag, Dictionary<PostId, SubReplyPost> parsedPostsInResponse)
     {

c#/crawler/src/Tieba/Crawl/Saver/BaseSaver.cs
@@ -39,8 +39,6 @@ public abstract class BaseSaver<TPost, TBaseRevision> : CommonInSavers<TBaseRevi
         where TRevision : class, IRevision
     {
         var dbSet = db.Set<TPost>().TagWith("ForUpdate");
-        if (dbSet == null) throw new ArgumentException(
-            $"DbSet<{typeof(TPost).Name}> is not exist in DbContext.");
 
         var existingPostsKeyById = dbSet.Where(existingPostPredicate).ToDictionary(postIdSelector);
         // deep copy before entities get mutated by CommonInSavers.SavePostsOrUsers()

c#/crawler/src/Tieba/Crawl/Saver/SaverChangeSet.cs
@@ -13,11 +13,9 @@ public class SaverChangeSet<T> where T : class, IPost
         var existingAfter = existingAfterAndNewlyAdded
             .IntersectBy(existingBefore.Select(postIdSelector), postIdSelector)
             .OrderBy(postIdSelector).ToList();
-        if (existingAfter.Count != existingBefore.Count) throw new(
-            "Length of existingAfter is not match with existingBefore.");
         Existing = new(existingBefore
             .OrderBy(postIdSelector)
-            .Zip(existingAfter, (before, after) => (before, after))
+            .EquiZip(existingAfter, (before, after) => (before, after))
             .ToList());
         NewlyAdded = new(existingAfterAndNewlyAdded
             .ExceptBy(existingBefore.Select(postIdSelector), postIdSelector)

c#/imagePipeline/src/Consumer/OcrConsumer.cs
@@ -28,8 +28,8 @@ public class OcrConsumer : MatrixConsumer
             Recognizer = result switch
             {
                 PaddleOcrRecognitionResult => "PaddleOCR",
-                TesseractRecognitionResult { IsVertical: false } => "TesseractHorizontal",
-                TesseractRecognitionResult { IsVertical: true } => "TesseractVertical",
+                TesseractRecognitionResult {IsVertical: false} => "TesseractHorizontal",
+                TesseractRecognitionResult {IsVertical: true} => "TesseractVertical",
                 _ => throw new ArgumentOutOfRangeException(nameof(result), result, null)
             },
             Confidence = result.Confidence,

c#/imagePipeline/src/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using System.ComponentModel.DataAnnotations;
 
 global using Autofac;
+global using CommunityToolkit.Diagnostics;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;

commit f6c5c021b893f337002d9dd0f0fb7dad42493be0

* fix wrongly flipped argument order for invoking `Guard.IsLessThanOrEqualTo()`, regression from commit 9694b66200462c5377cf85aef687f21cfab33d71 @ `crawler.ExtensionMethods.NextLong()`
* split codes related to class `OcrConsumer` into a new method `ConsumeOcrConsumerWithAllScrips()`
* move the loop over available scripts into method `ConsumeOcrConsumerWithAllScrips()` to reuse already download and decoded images across all scrips
@ `DoWork()`
* extract the querying for unconsumed images from db into a new local method `GetUnconsumedImages()`
* now will only query unconsumed images based on DbSet `ImageMetadata` that was inserted by class `MetadataConsumer`, instead of DbSet `ImageOcr(Boxes|Lines)` that was inserted by class `OcrConsumer` @ `GetUnconsumedImages()`
@ `ImageBatchGenerator()`
@ `imagePipeline.ImagePipelineWorker`
@ c#

c#/crawler/src/ExtensionMethods.cs
@@ -11,7 +11,7 @@ public static class ExtensionMethods
     /// <see>https://stackoverflow.com/questions/6651554/random-number-in-long-range-is-this-the-way/13095144#13095144</see>
     public static long NextLong(this Random random, long min, long max)
     {
-        Guard.IsLessThanOrEqualTo(max, min);
+        Guard.IsLessThanOrEqualTo(min, max);
 
         var uRange = (ulong)(max - min);
         ulong ulongRand;

c#/imagePipeline/src/ImagePipelineWorker.cs
@@ -21,48 +21,69 @@ public class ImagePipelineWorker : ErrorableWorker
 
     protected override async Task DoWork(CancellationToken stoppingToken)
     {
-        foreach (var script in new[] {"zh-Hans", "zh-Hant", "ja", "en"})
+        await foreach (var imageAndBytesKeyById in ImageBatchGenerator(stoppingToken))
         {
             await using var scope1 = _scope0.BeginLifetimeScope();
-            var db = scope1.Resolve<ImagePipelineDbContext.New>()(script);
-            var hashConsumer = scope1.Resolve<HashConsumer>();
-            var ocrConsumer = scope1.Resolve<OcrConsumer.New>()(script);
-            await ocrConsumer.InitializePaddleOcr(stoppingToken);
+            var db = scope1.Resolve<ImagePipelineDbContext.New>()("");
+            await using var transaction = await db.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted, stoppingToken);
 
-            await foreach (var imageAndBytesKeyById in ImageBatchGenerator(db, stoppingToken))
+            MetadataConsumer.Consume(db, imageAndBytesKeyById, stoppingToken);
+            var matricesKeyByImageId = imageAndBytesKeyById.ToDictionary(pair => pair.Key,
+                // preserve alpha channel if there's any, so the type of mat might be CV_8UC3 or CV_8UC4
+                pair => Cv2.ImDecode(pair.Value.Bytes, ImreadModes.Unchanged));
+            try
             {
-                await using var transaction = await db.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted, stoppingToken);
-                MetadataConsumer.Consume(db, imageAndBytesKeyById, stoppingToken);
-                var matricesKeyByImageId = imageAndBytesKeyById.ToDictionary(pair => pair.Key,
-                    // preserve alpha channel if there's any, so the type of mat might be CV_8UC3 or CV_8UC4
-                    pair => Cv2.ImDecode(pair.Value.Bytes, ImreadModes.Unchanged));
-                try
-                {
-                    hashConsumer.Consume(db, matricesKeyByImageId, stoppingToken);
-                    ocrConsumer.Consume(db, matricesKeyByImageId, stoppingToken);
-                }
-                finally
-                {
-                    matricesKeyByImageId.Values.ForEach(mat => mat.Dispose());
-                }
+                var hashConsumer = scope1.Resolve<HashConsumer>();
+                hashConsumer.Consume(db, matricesKeyByImageId, stoppingToken);
                 _ = await db.SaveChangesAsync(stoppingToken);
                 await transaction.CommitAsync(stoppingToken);
+                await ConsumeOcrConsumerWithAllScrips(scope1, matricesKeyByImageId, stoppingToken);
+            }
+            finally
+            {
+                matricesKeyByImageId.Values.ForEach(mat => mat.Dispose());
             }
         }
     }
 
+    private static async Task ConsumeOcrConsumerWithAllScrips
+        (ILifetimeScope scope, Dictionary<uint, Mat> matricesKeyByImageId, CancellationToken stoppingToken)
+    {
+        foreach (var script in new[] {"zh-Hans", "zh-Hant", "ja", "en"})
+        {
+            await using var scope1 = scope.BeginLifetimeScope();
+            var db = scope1.Resolve<ImagePipelineDbContext.New>()(script);
+            await using var transaction = await db.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted, stoppingToken);
+
+            var ocrConsumer = scope1.Resolve<OcrConsumer.New>()(script);
+            await ocrConsumer.InitializePaddleOcr(stoppingToken);
+            ocrConsumer.Consume(db, matricesKeyByImageId, stoppingToken);
+
+            _ = await db.SaveChangesAsync(stoppingToken);
+            await transaction.CommitAsync(stoppingToken);
+        }
+    }
+
     private async IAsyncEnumerable<Dictionary<ImageId, (TiebaImage Image, byte[] Bytes)>> ImageBatchGenerator
-        (ImagePipelineDbContext db, [EnumeratorCancellation] CancellationToken stoppingToken)
+        ([EnumeratorCancellation] CancellationToken stoppingToken)
     {
         ImageId lastImageIdInPreviousBatch = 0;
         while (true)
         {
-            var images = (from image in db.Images.AsNoTracking()
-                where image.ImageId > lastImageIdInPreviousBatch
-                      && !db.ImageOcrBoxes.Select(e => e.ImageId).Contains(image.ImageId)
-                      && !db.ImageOcrLines.Select(e => e.ImageId).Contains(image.ImageId)
-                orderby image.ImageId
-                select image).Take(_batchSize).ToList();
+            List<TiebaImage> GetUnconsumedImages()
+            { // dispose db inside scope1 after returned to prevent long running db connection
+                using var scope1 = _scope0.BeginLifetimeScope();
+                var db = scope1.Resolve<ImagePipelineDbContext.New>()("");
+                return (from image in db.Images.AsNoTracking()
+                        where image.ImageId > lastImageIdInPreviousBatch
+                              // this will not get images that have not yet been consumed by OcrConsumer
+                              // since the transaction in ConsumeOcrConsumerWithAllScrips() is de-synced with other consumer
+                              && !db.ImageMetadata.Select(e => e.ImageId).Contains(image.ImageId)
+                        orderby image.ImageId
+                        select image
+                    ).Take(_batchSize).ToList();
+            }
+            var images = GetUnconsumedImages();
             if (images.Any()) yield return new(
                 await Task.WhenAll(images.Select(async image =>
                     KeyValuePair.Create(image.ImageId, (image, await _imageRequester.GetImageBytes(image, stoppingToken))))));