378ca6a4490412005544550d5bdb5d6d745674b8
n0099 <n@n0099.net>
2024-01-01 12:36:23 +0800

    * add options for vite plugin `rollup-plugin-visualizer` & `vite-bundle-analyzer` @ vite.config.ts $ yarn add -D vite-bundle-analyzer @ fe

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,11 +1,16 @@
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
+import { analyzer } from 'vite-bundle-analyzer';
 import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), visualizer()],
+    plugins: [
+        vue(),
+        visualizer({ filename: 'dist/rollup-plugin-visualizer.html', gzipSize: true, brotliSize: true }),
+        analyzer({ analyzerMode: 'static', fileName: 'vite-bundle-analyzer' })
+    ],
     build: { target: 'esnext' },
     resolve: {
         alias: [
