253d695432d2bdb46f8c781ac8d2f3a11ff5cf2d
n0099 <n@n0099.net>
2024-01-01 12:36:23 +0800

    $ yarn add -D rollup-plugin-visualizer $ yarn upgrade-interactive @ fe

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), visualizer()],
     build: { target: 'esnext' },
     resolve: {
         alias: [
