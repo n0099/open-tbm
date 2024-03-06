n0099/open-tbm

commit 61b79730aef6e70fad6d98d020d0ddbb45225677

+ ref `queryParam` & `shouldFetch` * rename ref `currentRoute` to `lastFetchingRoute` * inline ref `title` and its mutating in `fetchPosts()` to its usage inside `useHead()` * move the scrolling viewport to `postListItemScrollPosition()` from `parseRouteThenFetch()` to `watchEffect()` - ref `postPages`, `isFetching` & `lastFetchError` in favor of `useApiPosts` - `v-(if|show)` directive on `<Placeholder(Error|PostList)>` @ `<Post>`
+ param `enabled` and wrap param `queryParam` in `ref<>` to enable reactivity @ `api/index.ts.useApi*()`
@ fe

fe/src/views/Post.vue
@@ -2,26 +2,26 @@
     <div class="container">
         <QueryForm ref="queryFormRef" :isLoading="isFetching" />
         <p>当前页数：{{ getRouteCursorParam(route) }}</p>
-        <Menu v-show="!_.isEmpty(postPages)" v-model:selectedKeys="selectedRenderTypes" mode="horizontal">
+        <Menu v-show="!_.isEmpty(data?.pages)" v-model:selectedKeys="selectedRenderTypes" mode="horizontal">
             <MenuItem key="list">列表视图</MenuItem>
             <MenuItem key="table">表格视图</MenuItem>
         </Menu>
     </div>
-    <div v-show="!_.isEmpty(postPages)" class="container-fluid">
+    <div v-if="!(data === undefined || _.isEmpty(data.pages))" class="container-fluid">
         <div class="row flex-nowrap">
-            <PostNav v-if="renderType === 'list'" :postPages="postPages" />
+            <PostNav v-if="renderType === 'list'" :postPages="data.pages" />
             <div class="post-page col mx-auto ps-0" :class="{ 'renderer-list': renderType === 'list' }">
-                <PostPage v-for="(posts, pageIndex) in postPages" :key="posts.pages.currentCursor"
-                          :renderType="renderType" :posts="posts"
-                          :currentRoute="currentRoute" :isLoadingNewPage="isFetching"
-                          :isLastPageInPages="pageIndex === postPages.length - 1" />
+                <PostPage v-for="(page, pageIndex) in data.pages" :key="page.pages.currentCursor"
+                          :renderType="renderType" :posts="page"
+                          :currentRoute="lastFetchingRoute" :isLoadingNewPage="isFetching"
+                          :isLastPageInPages="pageIndex === data.pages.length - 1" />
             </div>
             <div v-if="renderType === 'list'" class="col d-none d-xxl-block p-0" />
         </div>
     </div>
     <div class="container">
-        <PlaceholderError v-if="lastFetchError !== null" :error="lastFetchError" class="border-top" />
-        <PlaceholderPostList v-show="showPlaceholderPostList" :isLoading="isFetching" />
+        <PlaceholderError :error="error" class="border-top" />
+        <PlaceholderPostList :isLoading="isFetching" />
     </div>
 </template>
 
@@ -32,17 +32,18 @@ import QueryForm from '@/components/Post/queryForm/QueryForm.vue';
 import PlaceholderError from '@/components/placeholders/PlaceholderError.vue';
 import PlaceholderPostList from '@/components/placeholders/PlaceholderPostList.vue';
 
-import { apiPosts, isApiError } from '@/api';
-import type { ApiError, ApiPosts, Cursor } from '@/api/index.d';
+import { useApiPosts } from '@/api';
+import type { ApiPosts, Cursor } from '@/api/index.d';
 import { getReplyTitleTopOffset, postListItemScrollPosition } from '@/components/Post/renderers/rendererList';
 import { compareRouteIsNewQuery, getRouteCursorParam } from '@/router';
 import type { ObjUnknown } from '@/shared';
 import { notyShow, scrollBarWidth, titleTemplate } from '@/shared';
 import { useTriggerRouteUpdateStore } from '@/stores/triggerRouteUpdate';
 
-import { computed, nextTick, onMounted, ref } from 'vue';
+import { computed, nextTick, onMounted, ref, watchEffect } from 'vue';
 import type { RouteLocationNormalized } from 'vue-router';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { watchOnce } from '@vueuse/core';
 import { Menu, MenuItem } from 'ant-design-vue';
 import { useHead } from '@unhead/vue';
 import * as _ from 'lodash-es';
@@ -50,57 +51,47 @@ import * as _ from 'lodash-es';
 export type PostRenderer = 'list' | 'table';
 
 const route = useRoute();
-const title = ref<string>('帖子查询');
-const postPages = ref<ApiPosts[]>([]);
-const isFetching = ref<boolean>(false);
-const lastFetchError = ref<ApiError | null>(null);
-const showPlaceholderPostList = ref<boolean>(true);
+const queryParam = ref<ApiPosts['queryParam']>();
+const shouldFetch = ref<boolean>(false);
+const { data, error, isFetching, isFetchedAfterMount } = useApiPosts(queryParam, shouldFetch);
 const selectedRenderTypes = ref<[PostRenderer]>(['list']);
 const renderType = computed(() => selectedRenderTypes.value[0]);
 const queryFormRef = ref<InstanceType<typeof QueryForm>>();
-const currentRoute = ref<RouteLocationNormalized>(route);
-useHead({ title: computed(() => titleTemplate(title.value)) });
+const lastFetchingRoute = ref<RouteLocationNormalized>(route);
+useHead({
+    title: computed(() => titleTemplate((() => {
+        const firstPostPage = data.value?.pages[0];
+        if (firstPostPage === undefined)
+            return '帖子查询';
+
+        const forumName = `${firstPostPage.forum.name}吧`;
+        const threadTitle = firstPostPage.threads[0].title;
+        // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
+        switch (queryFormRef.value?.getCurrentQueryType()) {
+            case 'fid':
+            case 'search':
+                return `${forumName} - 帖子查询`;
+            case 'postID':
+                return `${threadTitle} - ${forumName} - 帖子查询`;
+        }
+
+        return '帖子查询';
+    })()))
+});
 
 const fetchPosts = async (queryParams: ObjUnknown[], isNewQuery: boolean, cursor: Cursor) => {
     const startTime = Date.now();
-    lastFetchError.value = null;
-    showPlaceholderPostList.value = true;
-    if (isNewQuery)
-        postPages.value = [];
-    isFetching.value = true;
-
-    const query = await apiPosts({
+    queryParam.value = {
         query: JSON.stringify(queryParams),
         cursor: isNewQuery ? undefined : cursor
-    }).finally(() => {
-        showPlaceholderPostList.value = false;
-        isFetching.value = false;
+    };
+    shouldFetch.value = true;
+    watchOnce(isFetchedAfterMount, value => {
+        if (value)
+            shouldFetch.value = false;
     });
-
-    if (isApiError(query)) {
-        lastFetchError.value = query;
-
+    if (error.value !== null)
         return false;
-    }
-    if (isNewQuery)
-        postPages.value = [query];
-    else
-        postPages.value.push(query); // todo: unshift when fetching previous page
-
-    const forumName = `${postPages.value[0].forum.name}吧`;
-    const threadTitle = postPages.value[0].threads[0].title;
-    switch (queryFormRef.value?.getCurrentQueryType()) {
-        case 'fid':
-        case 'search':
-            title.value = `${forumName} - 帖子查询`;
-            break;
-        case 'postID':
-            title.value = `${threadTitle} - ${forumName} - 帖子查询`;
-            break;
-        case 'empty':
-        case undefined:
-            throw new Error(queryFormRef.value?.getCurrentQueryType());
-    }
 
     const networkTime = Date.now() - startTime;
     await nextTick(); // wait for child components finish dom update
@@ -110,6 +101,7 @@ const fetchPosts = async (queryParams: ObjUnknown[], isNewQuery: boolean, cursor
 
     return true;
 };
+
 const scrollToPostListItem = (el: Element) => {
     // simply invoke el.scrollIntoView() for only once will scroll the element to the top of the viewport
     // and then some other elements above it such as img[loading='lazy'] may change its box size
@@ -133,41 +125,41 @@ const scrollToPostListItem = (el: Element) => {
     tryScroll();
     addEventListener('scrollend', tryScroll);
 };
+watchEffect(() => {
+    if (isFetchedAfterMount.value && renderType.value === 'list') {
+        const scrollPosition = postListItemScrollPosition(lastFetchingRoute.value);
+        if (scrollPosition === false)
+            return;
+        const el = document.querySelector(scrollPosition.el);
+        if (el === null)
+            return;
+        requestIdleCallback(function retry(deadline) {
+            if (deadline.timeRemaining() > 0)
+                scrollToPostListItem(el);
+            else
+                requestIdleCallback(retry);
+        });
+    }
+});
+
 const parseRouteThenFetch = async (newRoute: RouteLocationNormalized, isNewQuery: boolean, cursor: Cursor) => {
     if (queryFormRef.value === undefined)
         return false;
     const flattenParams = await queryFormRef.value.parseRouteToGetFlattenParams(newRoute);
     if (flattenParams === false)
         return false;
-    currentRoute.value = newRoute;
+    lastFetchingRoute.value = newRoute;
     const isFetchSuccess = await fetchPosts(flattenParams, isNewQuery, cursor);
-    if (isFetchSuccess && renderType.value === 'list') {
-        (() => {
-            const scrollPosition = postListItemScrollPosition(newRoute);
-            if (scrollPosition === false)
-                return;
-            const el = document.querySelector(scrollPosition.el);
-            if (el === null)
-                return;
-            requestIdleCallback(function retry(deadline) {
-                if (deadline.timeRemaining() > 0)
-                    scrollToPostListItem(el);
-                else
-                    requestIdleCallback(retry);
-            });
-        })();
-    }
 
     return isFetchSuccess;
 };
-
 onBeforeRouteUpdate(async (to, from) => {
     const isNewQuery = useTriggerRouteUpdateStore()
         .isTriggeredBy('<QueryForm>@submit', { ...to, force: true })
         || compareRouteIsNewQuery(to, from);
     const cursor = getRouteCursorParam(to);
     if (!(isNewQuery || _.isEmpty(_.filter(
-        postPages.value,
+        data.value?.pages,
         i => i.pages.currentCursor === cursor
     ))))
         return true;

fe/src/api/index.ts
@@ -1,4 +1,5 @@
 import type { Api, ApiError, ApiForums, ApiPosts, ApiStatsForumPostCount, ApiStatus, ApiUsers, Cursor, CursorPagination } from '@/api/index.d';
+import type { MaybeRefOrGetter, Ref } from 'vue';
 import type { InfiniteData, QueryFunctionContext, QueryKey } from '@tanstack/vue-query';
 import { useInfiniteQuery, useQuery } from '@tanstack/vue-query';
 import nprogress from 'nprogress';
@@ -80,22 +81,26 @@ const useApi = <
     TApi extends Api<TResponse, TQueryParam>,
     TResponse = TApi['response'],
     TQueryParam = TApi['queryParam']>
-(endpoint: string, requesterGetter: ReqesuterGetter) => (queryParam?: TQueryParam) =>
-    useQuery<TResponse, ApiErrorClass, TResponse>({
-        queryKey: [endpoint, queryParam],
-        queryFn: requesterGetter<TResponse, TQueryParam>(`/${endpoint}`, queryParam)
-    });
+(endpoint: string, requesterGetter: ReqesuterGetter) =>
+    (queryParam?: Ref<TQueryParam | undefined>, enabled?: MaybeRefOrGetter<boolean>) =>
+        useQuery<TResponse, ApiErrorClass, TResponse>({
+            queryKey: [endpoint, queryParam],
+            queryFn: requesterGetter<TResponse, TQueryParam>(`/${endpoint}`, queryParam?.value),
+            enabled
+        });
 const useApiWithCursor = <
     TApi extends Api<TResponse, TQueryParam>,
     TResponse = TApi['response'] & CursorPagination,
     TQueryParam = TApi['queryParam']>
-(endpoint: string, requesterGetter: ReqesuterGetter) => (queryParam?: TQueryParam) =>
-    useInfiniteQuery<TResponse & CursorPagination, ApiErrorClass, InfiniteData<TResponse & CursorPagination, Cursor>, QueryKey, Cursor>({
-        queryKey: [endpoint, queryParam],
-        queryFn: requesterGetter<TResponse & CursorPagination, TQueryParam>(`/${endpoint}`, queryParam),
-        initialPageParam: '',
-        getNextPageParam: lastPage => lastPage.pages.nextCursor
-    });
+(endpoint: string, requesterGetter: ReqesuterGetter) =>
+    (queryParam?: Ref<TQueryParam | undefined>, enabled?: MaybeRefOrGetter<boolean>) =>
+        useInfiniteQuery<TResponse & CursorPagination, ApiErrorClass, InfiniteData<TResponse & CursorPagination, Cursor>, QueryKey, Cursor>({
+            queryKey: [endpoint, queryParam],
+            queryFn: requesterGetter<TResponse & CursorPagination, TQueryParam>(`/${endpoint}`, queryParam?.value),
+            initialPageParam: '',
+            getNextPageParam: lastPage => lastPage.pages.nextCursor,
+            enabled
+        });
 
 export const useApiForums = () => useApi<ApiForums>('forums', getRequester)();
 export const useApiStatus = useApi<ApiStatus>('status', getRequesterWithReCAPTCHA);

commit d65bec292fa8e3ab2d108adc6d17c71fc277aec2

* fix functions within the provided `userProvision` are losing reactivity of `props.initialPosts.users` @ `<RendererList>` * now will continue scrolling up the viewport when the top edge of the scrolling post list item element is above the top of the viewport @ `scrollToPostListItem()@index.ts` @ components/Post/renderers/list
* fix not passing `props.modelValue` to `<select>` @ `components/widgets/<SelectForum>`

* fix always showing `<PlaceholderPostList>` without loading spinner due to the regression made in 61b79730aef6e70fad6d98d020d0ddbb45225677
* fix showing negative `(network|render)Time` when the result data of fetching query key is cached @ `fetchPosts()`
@ `<Post>`
@ fe

fe/src/components/Post/renderers/list/RendererList.vue
@@ -13,20 +13,21 @@ import type { Reply, SubReply, Thread } from '@/api/post';
 import { compareRouteIsNewQuery, setComponentCustomScrollBehaviour } from '@/router';
 import type { Modify } from '@/shared';
 import { initialTippy } from '@/shared/tippy';
+import type { ComputedRef } from 'vue';
 import { computed, onMounted, provide } from 'vue';
 import type { RouterScrollBehavior } from 'vue-router';
 import * as _ from 'lodash-es';
 
 const props = defineProps<{ initialPosts: ApiPosts['response'] }>();
-const getUser = baseGetUser(props.initialPosts.users);
-const renderUsername = baseRenderUsername(getUser);
+const getUser = computed(() => baseGetUser(props.initialPosts.users));
+const renderUsername = computed(() => baseRenderUsername(getUser.value));
 const userProvision = { getUser, renderUsername };
 
 // export type UserProvision = typeof userProvision;
 // will trigger @typescript-eslint/no-unsafe-assignment when `inject<UserProvision>('userProvision')`
 export interface UserProvision {
-    getUser: ReturnType<typeof baseGetUser>,
-    renderUsername: ReturnType<typeof baseRenderUsername>
+    getUser: ComputedRef<ReturnType<typeof baseGetUser>>,
+    renderUsername: ComputedRef<ReturnType<typeof baseRenderUsername>>
 }
 provide<UserProvision>('userProvision', userProvision);
 

fe/src/components/Post/renderers/list/index.ts
@@ -31,7 +31,7 @@ const scrollToPostListItem = (el: Element) => {
         // and the element reached the top of viewport
         const elTop = el.getBoundingClientRect().top;
         const replyTitleTopOffset = getReplyTitleTopOffset();
-        if (Math.abs(elTop) < replyTitleTopOffset + (window.innerHeight * 0.05)) // at most 5dvh tolerance
+        if (elTop > 0 && Math.abs(elTop) < replyTitleTopOffset + (window.innerHeight * 0.05)) // at most 5dvh tolerance
             removeEventListener('scrollend', tryScroll);
         else
             document.documentElement.scrollBy({ top: elTop - replyTitleTopOffset });

fe/src/components/widgets/SelectForum.vue
@@ -2,7 +2,7 @@
     <slot v-if="isFetching || isError" :renderer="indicatorsRenderer" name="indicators">
         <RenderFunction :renderer="indicatorsRenderer" />
     </slot>
-    <select v-if="!isFetching && isSuccess && data !== undefined"
+    <select v-if="!isFetching && isSuccess && data !== undefined" v-model="modelValue"
             class="form-select form-control" v-bind="$attrs">
         <option value="0">未指定</option>
         <option v-for="forum in data"
@@ -13,11 +13,13 @@
 <script setup lang="tsx">
 import { useApiForums } from '@/api/index';
 import RenderFunction from '@/components/RenderFunction';
+import type { Fid } from '@/shared';
 import type { VNode } from 'vue';
 import { computed } from 'vue';
 
 defineOptions({ inheritAttrs: false });
 defineSlots<{ indicators: (props: { renderer: VNode }) => unknown }>();
+const modelValue = defineModel<Fid>();
 
 const { data, error, isFetching, isError, isSuccess } = useApiForums();
 const indicatorsRenderer = computed(() => (<>

fe/src/views/Post.vue
@@ -21,7 +21,7 @@
     </div>
     <div class="container">
         <PlaceholderError :error="error" class="border-top" />
-        <PlaceholderPostList :isLoading="isFetching" />
+        <PlaceholderPostList v-show="isPending" :isLoading="isFetching" />
     </div>
 </template>
 
@@ -36,7 +36,7 @@ import { useApiPosts } from '@/api';
 import type { ApiPosts, Cursor } from '@/api/index.d';
 import { scrollToPostListItemByRoute } from '@/components/Post/renderers/list/index';
 import { compareRouteIsNewQuery, getRouteCursorParam } from '@/router';
-import type { ObjUnknown } from '@/shared';
+import type { ObjUnknown, UnixTimestamp } from '@/shared';
 import { notyShow, scrollBarWidth, titleTemplate } from '@/shared';
 import { useTriggerRouteUpdateStore } from '@/stores/triggerRouteUpdate';
 
@@ -53,7 +53,8 @@ export type PostRenderer = 'list' | 'table';
 const route = useRoute();
 const queryParam = ref<ApiPosts['queryParam']>();
 const shouldFetch = ref<boolean>(false);
-const { data, error, isFetching, isFetchedAfterMount, dataUpdatedAt, errorUpdatedAt } = useApiPosts(queryParam, shouldFetch);
+const { data, error, isPending, isRefetching, isFetching, isFetchedAfterMount, dataUpdatedAt, errorUpdatedAt } =
+    useApiPosts(queryParam, shouldFetch);
 const selectedRenderTypes = ref<[PostRenderer]>(['list']);
 const renderType = computed(() => selectedRenderTypes.value[0]);
 const queryFormRef = ref<InstanceType<typeof QueryForm>>();
@@ -90,16 +91,26 @@ const fetchPosts = (queryParams: ObjUnknown[], cursor: Cursor) => {
         if (value)
             shouldFetch.value = false;
     });
-    watchOnce([dataUpdatedAt, errorUpdatedAt], async updatedAt => {
-        const networkTime = (_.max(updatedAt) ?? 0) - startTime;
-        await nextTick(); // wait for child components finish dom update
+    const updatedAtWatcher = async (updatedAt: UnixTimestamp[]) => {
+        if (isRefetching.value) // background refetching with cached data already presented, watching for refetch
+            watchOnce([dataUpdatedAt, errorUpdatedAt], updatedAtWatcher);
+        const maxUpdatedAt = Math.max(...updatedAt);
+        if (maxUpdatedAt === 0) { // just starts to fetch, defer watching to next time
+            watchOnce([dataUpdatedAt, errorUpdatedAt], updatedAtWatcher);
+
+            return;
+        }
+        const isCached = maxUpdatedAt < startTime;
+        const networkTime = isCached ? 0 : maxUpdatedAt - startTime;
+        await nextTick(); // wait for child components to finish dom update
         const fetchedPage = data.value?.pages.find(i => i.pages.currentCursor === cursor);
         const postCount = _.sum(Object.values(fetchedPage?.pages.matchQueryPostCount ?? {}));
         const renderTime = (Date.now() - startTime - networkTime) / 1000;
         notyShow('success', `已加载${postCount}条记录
             前端耗时${renderTime.toFixed(2)}s
-            后端+网络耗时${(networkTime / 1000).toFixed(2)}s`);
-    });
+            ${isCached ? '使用本地缓存' : `后端+网络耗时${(networkTime / 1000).toFixed(2)}s`}`);
+    };
+    watchOnce([dataUpdatedAt, errorUpdatedAt], updatedAtWatcher);
 };
 
 watch(isFetchedAfterMount, async () => {