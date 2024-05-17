c129bf200fc26a3e6471ec3a712f5b832fd6082d
n0099 <n@n0099.net>
2024-05-17 22:38:21 +0800

    * fix may concurrently insert duplicate `ImageInReply` entities by locking the global placeholder object of each image URL filename * fix entities `ReplyContentImage` related with already locked image URL filename will not be inserted * fix never release locked `SaverLocks` before returning @ `Save()`
    + static field `LocksKeyByUrlFilename`
    @ ReplyContentImageSaver.cs
    @ c#/crawler

--- c#/crawler/src/Tieba/Crawl/Saver/ReplyContentImageSaver.cs
+++ c#/crawler/src/Tieba/Crawl/Saver/ReplyContentImageSaver.cs
@@ -2,6 +2,8 @@ namespace tbm.Crawler.Tieba.Crawl.Saver;
 
 public class ReplyContentImageSaver(SaverLocks<string> locks)
 {
+    private static readonly ConcurrentDictionary<string, object> LocksKeyByUrlFilename = new();
+
     public void Save(CrawlerDbContext db, IEnumerable<ReplyPost> replies)
     {
         var pidAndImageList = (
@@ -18,23 +20,38 @@ public class ReplyContentImageSaver(SaverLocks<string> locks)
             .DistinctBy(t => (t.Pid, t.Image.UrlFilename))
             .ToList();
         if (pidAndImageList.Count == 0) return;
-
         var imagesKeyByUrlFilename = pidAndImageList.Select(t => t.Image)
             .DistinctBy(image => image.UrlFilename).ToDictionary(image => image.UrlFilename);
+
         var existingImages = (
                 from e in db.ImageInReplies.AsTracking()
                 where imagesKeyByUrlFilename.Keys.Contains(e.UrlFilename)
                 select e)
             .ToDictionary(e => e.UrlFilename);
+        var newImages = imagesKeyByUrlFilename.ExceptByKey(existingImages.Keys).Keys().ToList();
+        var newlyLocked = locks.AcquireLocks(newImages);
+        var alreadyLocked = newImages.Except(newlyLocked).ToList();
+
+        if (newlyLocked.Any(urlFilename => !LocksKeyByUrlFilename.TryAdd(urlFilename, new())))
+            throw new InvalidOperationException();
+        alreadyLocked.ForEach(urlFilename =>
+        {
+            lock (LocksKeyByUrlFilename[urlFilename]) { }
+        });
+        existingImages = existingImages
+            .Concat((
+                from e in db.ImageInReplies.AsTracking()
+                where alreadyLocked.Contains(e.UrlFilename)
+                select e).ToDictionary(e => e.UrlFilename))
+            .ToDictionary();
+
         (from existing in existingImages.Values
                 where existing.ExpectedByteSize == 0 // randomly respond with 0
                 join newInContent in imagesKeyByUrlFilename.Values
                     on existing.UrlFilename equals newInContent.UrlFilename
                 select (existing, newInContent))
             .ForEach(t => t.existing.ExpectedByteSize = t.newInContent.ExpectedByteSize);
-        var newImagesUrlFilename = imagesKeyByUrlFilename.ExceptByKey(existingImages.Keys).Keys().ToList();
         db.ReplyContentImages.AddRange(pidAndImageList
-            .ExceptBy(locks.AcquireLocks(newImagesUrlFilename), t => t.Image.UrlFilename)
             .Select(t => new ReplyContentImage
             {
                 Pid = t.Pid,
@@ -49,5 +66,9 @@ public class ReplyContentImageSaver(SaverLocks<string> locks)
                     ? e
                     : imagesKeyByUrlFilename[t.Image.UrlFilename]
             }));
+
+        if (newlyLocked.Any(urlFilename => !LocksKeyByUrlFilename.TryRemove(urlFilename, out _)))
+            throw new InvalidOperationException();
+        locks.Dispose();
     }
 }
