804da48f27d24507bb7cf9c3fba7887c7c362646
n0099 <n@n0099.net>
2024-06-10 15:15:05 +0800

    * now display the original response json within npm package `@microlink/react-json-view` instead of composer package `symfony/var-dumper` when the value of `Accept-Encoding` request header is `text/html` * now will pretty print the response json for any other  values of `Accept-Encoding` request header @ `App\Http\Middleware\DumpJsonResponse->handle()` @ be
    + .git-blame-ignore-revs

--- be/app/Http/Middleware/DumpJsonResponse.php
+++ be/app/Http/Middleware/DumpJsonResponse.php
@@ -14,10 +14,21 @@ class DumpJsonResponse
     public function handle(Request $request, \Closure $next): mixed
     {
         $response = $next($request);
-        if ($response instanceof JsonResponse && $request->accepts('text/html')) {
+        if ($response instanceof JsonResponse) {
+            if ($request->accepts('text/html')) {
+                return response(<<<HTML
+                    <div id="root"></div>
+                    <script type="module">
+                        import ReactJsonView from 'https://cdn.jsdelivr.net/npm/@microlink/react-json-view@1.23.0/+esm';
+                        import { createElement } from 'https://cdn.jsdelivr.net/npm/react@18.3.1/+esm';
+                        import { createRoot } from 'https://cdn.jsdelivr.net/npm/react-dom@18.3.1/+esm';
+
+                        const root = createRoot(document.getElementById('root'));
+                        root.render(createElement(ReactJsonView.default, { src: $response->content(), quotesOnKeys: false }));
+                    </script>
+                    HTML);
+            }
             $response->setEncodingOptions(JSON_PRETTY_PRINT);
-            dump($response->content());
-            return response('');
         }
 
         return $response;
