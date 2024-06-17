3c47d41e924fe9e6ff5f1e7d04104238871d395d
n0099 <n@n0099.net>
2024-06-17 06:35:40 +0000

    * disable `pathPrefix` when auto importing components * auto importing `api/**` * enable module `@pinia/nuxt` @ nuxt.config.ts
    $ yarn add @pinia/nuxt
    $ rm tsconfig.node.json
    @ fe

--- fe/nuxt.config.ts
+++ fe/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    modules: ['@pinia/nuxt'],
+    components: [
+        {
+            path: '~/components',
+            pathPrefix: false
+        }
+    ],
+    imports: {
+        dirs: ['api/**']
+    }
+});
