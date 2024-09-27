dae03d1d207631fdffb09c11bd3c9b5769e6bed8
n0099 <n@n0099.net>
2024-09-27 09:43:25 +0000

    $ composer require --dev symfony/maker-bundle @ symfony

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
