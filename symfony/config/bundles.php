46c22885c322c67b1e08150ea7ac18b2f2c0bf6a
n0099 <n@n0099.net>
2024-09-27 09:42:16 +0000

    $ composer require --dev symfony/debug-bundle @ symfony

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
