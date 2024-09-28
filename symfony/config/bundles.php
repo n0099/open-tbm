97d8491d2635a7f6e9ba6a53351a896f8ab6099b
n0099 <n@n0099.net>
2024-09-28 05:19:46 +0000

    $ composer require symfony/twig-bundle # already required by symfony/web-profiler-bundle $ composer require illuminate/collections # also required by spatie/symfony-ignition-bundle $ composer require --dev spatie/symfony-ignition-bundle # to fix https://github.com/symfony/symfony/discussions/46457 as `highlight_file()` can be dangerous: https://book.hacktricks.xyz/network-services-pentesting/pentesting-web/php-tricks-esp/php-useful-functions-disable_functions-open_basedir-bypass#filesystem-functions @ symfony

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Spatie\SymfonyIgnitionBundle\IgnitionBundle::class => ['dev' => true],
 ];
