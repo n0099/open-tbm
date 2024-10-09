b531590299076757fd605cef1fc98b016b2e4a63
n0099 <n@n0099.net>
2024-10-09 18:19:47 +0000

    $ rm -r be && mv symfony be

--- be/tests/TestCase.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace Tests;
-
-// @phpcs:ignore PSR2.Classes.ClassDeclaration.OpenBraceNewLine -- https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/566
-abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase {}
