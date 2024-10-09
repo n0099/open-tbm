0302713d570b05bc0d2be37fe183eca2cf478cfc
n0099 <n@n0099.net>
2024-10-09 01:30:22 +0000

    * migrate config files of php-cs-fixer, pint, phpmd, phpstan, psalm, phan, phpunit & infection from `be` to `symfony`

--- symfony/.php-cs-fixer.dist.php
+++ symfony/.php-cs-fixer.dist.php
@@ -1,13 +1,10 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-;
-
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@Symfony' => true,
+        '@PER-CS2.0' => true,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder((new PhpCsFixer\Finder())
+        ->in(__DIR__)
+        ->ignoreVCSIgnored(true)
+        ->ignoreDotFiles(false));
