6cfd50a9112d0a0c114b8a8dd55b9f8405e8af52
n0099 <n@n0099.net>
2025-05-08 20:43:26 +0000

    * replace interpolating raw SQL with DQL @ `App\Repository\Post\ThreadRepository->getThreadsIdWithMaxPostedAtAfter()` * register custom DQL function `DoctrineExtensions\Query\Mysql\Greatest` @ config/package/doctrine.yaml @ be

--- be/src/Repository/Post/ThreadRepository.php
+++ be/src/Repository/Post/ThreadRepository.php
@@ -3,8 +3,6 @@
 namespace App\Repository\Post;
 
 use App\DTO\PostKey\Thread as ThreadKey;
-use App\Entity\Post\Reply;
-use App\Entity\Post\SubReply;
 use App\Entity\Post\Thread;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
@@ -53,27 +51,18 @@ class ThreadRepository extends PostRepository
 
     public function getThreadsIdWithMaxPostedAtAfter(int $fid, int $after, int $limit): array
     {
-        $entityManager = $this->getEntityManager();
-        $threadTable = $entityManager->getClassMetadata(Thread::class)->getTableName();
-        $replyTable = $entityManager->getClassMetadata(Reply::class)->getTableName();
-        $subReplyTable = $entityManager->getClassMetadata(SubReply::class)->getTableName();
-        $statement = $entityManager->getConnection()->prepare(<<<"SQL"
-            SELECT t.tid,
-                GREATEST(t."postedAt", MAX(r."postedAt"), MAX(sr."postedAt")) AS "maxPostedAt"
-            FROM $threadTable t
-                JOIN $replyTable r ON r.tid = t.tid
-                JOIN $subReplyTable sr ON sr.pid = r.pid
-            WHERE t.fid = :fid
-                AND r.fid = :fid
-                AND sr.fid = :fid
-                AND t.tid > :after
-            GROUP BY t.tid
-            ORDER BY t.tid
-            LIMIT :limit
-            SQL);
-        $statement->bindValue('fid', $fid);
-        $statement->bindValue('after', $after);
-        $statement->bindValue('limit', $limit);
-        return $statement->executeQuery()->fetchAllAssociative();
+        $dql = 'SELECT t.tid,
+                    GREATEST(t.postedAt, MAX(r.postedAt), MAX(sr.postedAt)) maxPostedAt
+                FROM App\Entity\Post\Thread t
+                    JOIN App\Entity\Post\Reply r WITH r.tid = t.tid
+                    JOIN App\Entity\Post\SubReply sr WITH sr.pid = r.pid
+                WHERE t.fid = :fid
+                    AND r.fid = :fid
+                    AND sr.fid = :fid
+                    AND t.tid > :after
+                GROUP BY t.tid
+                ORDER BY t.tid';
+        return $this->createQueryWithParams($dql, ['fid' => $fid, 'after' => $after])
+            ->setMaxResults($limit)->getResult();
     }
 }
