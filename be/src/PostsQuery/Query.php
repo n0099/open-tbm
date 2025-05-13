n0099/open-tbm

commit 70930bb5bee29047ef9274f64e462cd8131038b7

- class `IndexQuery` for renaming class `SearchQuery` to `Query` * now will response with error code `40406` when the forum of requested `fid` doesn't exists, like `IndexQuery->query()` @ `Query->query()` @ `App\PostsQuery`
* inline variables `$isFidParamNull` & `$isQueryByFid` into its only usage in `$isSearchQuery
- unused variable `$isSearchQuery`
@ `query()`

* replace service locator with injected services @ `__construct()`
@ `App\Controller\PostsController`

- error code `40002` as its only usage has been removed @ `App\Helper::ERROR_STATUS_CODE_INFO`
@ be

be/src/PostsQuery/Query.php
@@ -2,17 +2,20 @@
 
 namespace App\PostsQuery;
 
+use App\Helper;
+use App\Repository\ForumRepository;
 use App\Repository\Post\PostRepository;
 use App\Repository\Post\PostRepositoryFactory;
 use App\Repository\UserRepository;
 use Doctrine\ORM\QueryBuilder;
 use Illuminate\Support\Collection;
 
-readonly class SearchQuery extends BaseQuery
+readonly class Query extends BaseQuery
 {
     public function __construct(
         QueryResult $queryResult,
         PostsTree $postsTree,
+        private ForumRepository $forumRepository,
         private PostRepositoryFactory $postRepositoryFactory,
         private UserRepository $userRepository,
     ) {
@@ -23,6 +26,7 @@ readonly class SearchQuery extends BaseQuery
     {
         /** @var int $fid */
         $fid = $params->getUniqueParamValue('fid');
+        Helper::abortAPIIfNot(40406, $this->forumRepository->isForumExists($fid));
 
         $orderByParam = $params->pick('orderBy')[0];
         $this->setOrderByField($orderByParam->value === 'default' ? 'postedAt' : $orderByParam->value)

be/src/PostsQuery/QueryResult.php
@@ -85,7 +85,7 @@ readonly class QueryResult
             );
             $qb->andWhere($qb->expr()->orX(...$comparisons));
             $cursors->mapWithKeys(fn($fieldValue, string $fieldName) =>
-            $qb->setParameter("cursor_$fieldName", $fieldValue)); // prevent overwriting existing param
+                $qb->setParameter("cursor_$fieldName", $fieldValue)); // prevent overwriting existing param
         });
 
         $resultsAndHasMorePages = $queries->map(fn(QueryBuilder $query) =>

be/src/Controller/PostsController.php
@@ -8,10 +8,8 @@ use App\DTO\User\User;
 use App\Entity\Post\Post;
 use App\Entity\Post\Thread;
 use App\Helper;
-use App\PostsQuery\BaseQuery;
-use App\PostsQuery\IndexQuery;
 use App\PostsQuery\ParamsValidator;
-use App\PostsQuery\SearchQuery;
+use App\PostsQuery\Query;
 use App\Repository\ForumRepository;
 use App\Repository\LatestReplierRepository;
 use App\Repository\Revision\AuthorExpGradeRepository;
@@ -19,9 +17,7 @@ use App\Repository\Revision\ForumModeratorRepository;
 use App\Repository\UserRepository;
 use App\Validator\Validator;
 use Illuminate\Support\Collection;
-use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Stopwatch\Stopwatch;
@@ -37,12 +33,8 @@ class PostsController extends AbstractController
         private readonly LatestReplierRepository $latestReplierRepository,
         private readonly ForumModeratorRepository $forumModeratorRepository,
         private readonly AuthorExpGradeRepository $authorExpGradeRepository,
-        #[AutowireLocator([
-            ParamsValidator::class,
-            IndexQuery::class,
-            SearchQuery::class,
-        ])]
-        private readonly ContainerInterface $locator,
+        private readonly ParamsValidator $paramsValidator,
+        private readonly Query $query,
     ) {}
 
     #[Route('/api/posts')]
@@ -56,8 +48,9 @@ class PostsController extends AbstractController
             )),
             'query' => new Assert\Required(new Assert\Json()),
         ]));
-        $validator = $this->locator->get(ParamsValidator::class);
-        $params = $validator->setParams(\Safe\json_decode($request->query->get('query'), true))->getParams();
+        $params = $this->paramsValidator
+            ->setParams(\Safe\json_decode($request->query->get('query'), true))
+            ->getParams();
 
         $postIDParams = $params->pick(...Helper::POST_ID);
         $isQueryByPostID =
@@ -67,29 +60,24 @@ class PostsController extends AbstractController
             && \count($postIDParams) === 1
             // is all post ID params doesn't own any sub param
             && array_filter($postIDParams, static fn($p) => $p->getAllSub() !== []) === [];
-        $isFidParamNull = $params->getUniqueParamValue('fid') === null;
         // is the fid param exists and there's no other params except unique params
-        $isQueryByFid = !$isFidParamNull && \count($params->omit(...ParamsValidator::UNIQUE_PARAMS_NAME)) === 0;
-        $isIndexQuery = $isQueryByPostID || $isQueryByFid;
-        $isSearchQuery = !$isIndexQuery;
-        Helper::abortAPIIf(40002, $isSearchQuery && $isFidParamNull);
-
-        $validator->addDefaultParamsThenValidate(shouldSkip40003: $isIndexQuery);
+        $isIndexQuery = $isQueryByPostID
+            || (!($params->getUniqueParamValue('fid') === null)
+                && \count($params->omit(...ParamsValidator::UNIQUE_PARAMS_NAME)) === 0);
+        $this->paramsValidator->addDefaultParamsThenValidate(shouldSkip40003: $isIndexQuery);
 
         $this->stopwatch->start('$queryClass->query()');
-        /** @var BaseQuery $query */
-        $query = $this->locator->get($isIndexQuery ? IndexQuery::class : SearchQuery::class);
-        $query->query($params, $request->query->get('cursor'));
+        $this->query->query($params, $request->query->get('cursor'));
         $this->stopwatch->stop('$queryClass->query()');
         $this->stopwatch->start('fillWithParentPost');
-        $matchQueryPostCounts = $query->postsTree->fillWithParentPost($query->queryResult);
+        $matchQueryPostCounts = $this->query->postsTree->fillWithParentPost($this->query->queryResult);
         $this->stopwatch->stop('fillWithParentPost');
 
         $this->stopwatch->start('queryUsers');
         $latestRepliers = $this->latestReplierRepository->getLatestRepliersWithoutNameWhenHasUid(
-            $query->postsTree->threads->map(fn(Thread $thread) => $thread->getLatestReplierId()),
+            $this->query->postsTree->threads->map(fn(Thread $thread) => $thread->getLatestReplierId()),
         );
-        $uids = collect([$query->postsTree->threads, $query->postsTree->replies, $query->postsTree->subReplies])
+        $uids = collect([$this->query->postsTree->threads, $this->query->postsTree->replies, $this->query->postsTree->subReplies])
             ->flatMap(static fn(Collection $posts) => $posts->map(fn(Post $post) => $post->getAuthorUid()))
             ->concat($latestRepliers->pluck('uid')->filter()) // filter() will remove NULLs
             ->unique();
@@ -98,7 +86,7 @@ class PostsController extends AbstractController
         $this->stopwatch->stop('queryUsers');
 
         $this->stopwatch->start('queryUserRelated');
-        $fid = $query->queryResult->fid;
+        $fid = $this->query->queryResult->fid;
         $authorExpGrades = collect($this->authorExpGradeRepository->getLatestOfUsers($fid, $uids))
             ->keyBy(fn(AuthorExpGrade $authorExpGrade) => $authorExpGrade->uid);
         $users->each(fn(User $user) => $user->setCurrentAuthorExpGrade($authorExpGrades[$user->getUid()]));
@@ -112,15 +100,15 @@ class PostsController extends AbstractController
         return [
             'type' => $isIndexQuery ? 'index' : 'search',
             'pages' => [
-                'currentCursor' => $query->queryResult->currentCursor,
-                'nextCursor' => $query->queryResult->nextCursor,
+                'currentCursor' => $this->query->queryResult->currentCursor,
+                'nextCursor' => $this->query->queryResult->nextCursor,
                 ...$matchQueryPostCounts,
             ],
             'forum' => $this->forumRepository->getForum($fid),
-            'threads' => $query->postsTree->reOrderNestedPosts(
-                $query->postsTree->nestPostsWithParent(),
-                $query->orderByField,
-                $query->orderByDesc,
+            'threads' => $this->query->postsTree->reOrderNestedPosts(
+                $this->query->postsTree->nestPostsWithParent(),
+                $this->query->orderByField,
+                $this->query->orderByDesc,
             ),
             'users' => $users,
             'latestRepliers' => $latestRepliers,

be/src/Helper.php
@@ -61,7 +61,6 @@ class Helper
         400 => [
             // 40000 => App\Exceptions\Handler::convertValidationExceptionToResponse()
             40001 => '帖子查询类型必须为索引或搜索查询',
-            40002 => '搜索查询必须指定查询贴吧',
             40003 => '部分查询参数与查询帖子类型要求不匹配',
             40004 => '排序方式与查询帖子类型要求不匹配',
             40005 => '提供了多个唯一查询参数',

be/src/PostsQuery/IndexQuery.php
@@ -1,78 +1,0 @@
-<?php
-
-namespace App\PostsQuery;
-
-use App\Repository\ForumRepository;
-use App\Repository\Post\PostRepository;
-use App\Repository\Post\PostRepositoryFactory;
-use App\Helper;
-use Doctrine\ORM\QueryBuilder;
-use Illuminate\Support\Collection;
-
-readonly class IndexQuery extends BaseQuery
-{
-    public function __construct(
-        QueryResult $queryResult,
-        PostsTree $postsTree,
-        private PostRepositoryFactory $postRepositoryFactory,
-        private ForumRepository $forumRepository,
-    ) {
-        parent::__construct($queryResult, $postsTree);
-    }
-
-    /** @SuppressWarnings(PHPMD.ElseExpression) */
-    public function query(QueryParams $params, ?string $cursor): void
-    {
-        /** @var array<string, mixed> $flatParams key by param name */
-        $flatParams = array_reduce(
-            $params->pick(...ParamsValidator::UNIQUE_PARAMS_NAME, ...Helper::POST_ID),
-            static fn(array $accParams, QueryParam $param) =>
-                [...$accParams, $param->name => $param->value, ...$param->getAllSub()],
-            [],
-        ); // flatten unique query params
-        /** @var Collection<string, int> $postIDParam key by post ID name, should contains only one param */
-        $postIDParam = collect($flatParams)->only(Helper::POST_ID);
-        $postIDParamName = $postIDParam->keys()->first();
-        $postIDParamValue = $postIDParam->first();
-        $hasPostIDParam = $postIDParam->count() === 1;
-        /** @var array<string> $postTypes */
-        $postTypes = $flatParams['postTypes'];
-
-        if ($flatParams['orderBy'] === 'default') {
-            $this->setOrderByField('postedAt'); // order by postedAt to prevent posts out of order when order by post ID
-            if (\array_key_exists('fid', $flatParams) && $postIDParam->count() === 0) { // query by fid only
-                $this->setOrderByDesc(true);
-            } elseif ($hasPostIDParam) { // query by post ID (with or without fid)
-                $this->setOrderByDesc(false);
-            }
-        }
-
-        /** @var Collection<string, QueryBuilder> $queries key by post type */
-        $queries = collect($this->postRepositoryFactory->newForumPosts())
-            ->only($postTypes)
-            ->transform(fn(PostRepository $repository) => $repository
-                ->selectPostKeyDTO($this->orderByField));
-
-        Helper::abortAPIIf(40406, array_key_exists('fid', $flatParams)
-            && !$this->forumRepository->isForumExists($flatParams['fid']));
-
-        if ($hasPostIDParam) {
-            $queries = $queries
-                ->only(\array_slice(
-                    Helper::POST_TYPES, // only query post types that own the querying post ID param
-                    array_search($postIDParamName, Helper::POST_ID, true),
-                ))
-                ->each(static fn(QueryBuilder $qb, string $type) =>
-                    $qb->where("t.$postIDParamName = :postIDParamValue")
-                        ->setParameter('postIDParamValue', $postIDParamValue));
-        }
-
-        $this->queryResult->setResult(
-            $queries,
-            $cursor,
-            $this->orderByField,
-            $this->orderByDesc,
-            queryByPostIDParamName: $hasPostIDParam ? $postIDParamName : null,
-        );
-    }
-}

commit 0d302008a5bf654b743b73fa88a895c9599417c2

* fix a regression since 70930bb5bee29047ef9274f64e462cd8131038b7 that always show cursor of all post types in the encoded next cursor @ `App\PostsQuery\Query->query()` * now can ignore multiple post type when encoding the next cursor @ `App\PostsQuery\QueryResult->setResult()` @ be

be/src/PostsQuery/Query.php
@@ -58,7 +58,15 @@ readonly class Query extends BaseQuery
                 return $postQuery;
             });
 
-        $this->queryResult->setResult($queries, $cursor, $this->getOrderByField(), $this->isOrderByDesc());
+        $queryByPostIDParamsName = collect(array_count_values(
+            collect($params->pick(...Helper::POST_ID))
+                ->filter(static fn(QueryParam $p) => $p->getSub('range') === '=')
+                ->map(static fn(QueryParam $p) => $p->name)
+                ->toArray()
+        ))
+            ->filter(static fn(int $counts) => $counts === 1)
+            ->keys();
+        $this->queryResult->setResult($queries, $cursor, $this->getOrderByField(), $this->isOrderByDesc(), $queryByPostIDParamsName);
     }
 
     /**

be/src/PostsQuery/QueryResult.php
@@ -52,7 +52,7 @@ readonly class QueryResult
         ?string $cursorParamValue,
         string $orderByField,
         bool $orderByDesc,
-        ?string $queryByPostIDParamName = null,
+        Collection $queryByPostIDParamsName,
     ): void {
         $this->stopwatch->start('setResult');
 
@@ -73,10 +73,10 @@ readonly class QueryResult
                 // https://slack.engineering/evolving-api-pagination-at-slack/
                 ->addOrderBy('t.' . Helper::POST_TYPE_TO_ID[$postType]);
 
-            $cursors = $cursorsKeyByPostType?->get($postType);
-            if ($cursors === null) {
+            if ($cursorsKeyByPostType === null) {
                 return;
             }
+            $cursors = $cursorsKeyByPostType->get($postType);
             $cursors = collect($cursors);
             $comparisons = $cursors->keys()->map(
                 fn(string $fieldName): Comparison => $orderByDesc
@@ -105,11 +105,9 @@ readonly class QueryResult
         $this->currentCursor = $cursorParamValue ?? '';
         $this->nextCursor = $resultsAndHasMorePages->pluck('hasMorePages')
             ->filter()->isNotEmpty() // filter() remove falsy
-            ? $this->cursorCodec->encodeNextCursor(
-                $queryByPostIDParamName === null
-                    ? $postsKeyByTypePluralName
-                    : $postsKeyByTypePluralName->except([Helper::POST_ID_TO_TYPE_PLURAL[$queryByPostIDParamName]]),
-            )
+            ? $this->cursorCodec->encodeNextCursor($postsKeyByTypePluralName->except(
+                $queryByPostIDParamsName->map(static fn(string $postID) => Helper::POST_ID_TO_TYPE_PLURAL[$postID])
+            ))
             : null;
 
         $this->stopwatch->stop('setResult');