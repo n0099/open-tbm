3c6d831387d62049496cbea8589d8511d6a19b18
n0099 <n@n0099.net>
2025-05-30 05:56:05 +0000

    $ yarn add -D stylelint-actions-formatters + `stylelint.config.ts` that extends `stylelint-config-recommended(-vue)?` * add `vue` to `stylelint.validate` and disable the vscode built-in css linter @ .vscode/settings.json @ fe
    + job `stylelint` @ .github/workflows/fe_base.yml

---
+++ fe/stylelint.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'stylelint';
+
+export default {
+    extends: [
+        'stylelint-config-recommended',
+        'stylelint-config-recommended-vue'
+    ]
+} as Config;
