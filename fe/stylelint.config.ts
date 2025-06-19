706446bc0cce7f10f83841fe0ad14231a26aa553
n0099 <n@n0099.net>
2025-06-19 14:14:27 +0000

    * now extending from preset `stylelint-config-standard(-vue)?` instead of `stylelint-recommended-standard(-vue)?` @ `stylelint.config.ts` $ yarn remove stylelint-recommended-standard-vue $ yarn add -D stylelint-config-standard-vue @ fe

--- fe/stylelint.config.ts
+++ fe/stylelint.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'stylelint';
 
 export default {
     extends: [
-        'stylelint-config-recommended',
-        'stylelint-config-recommended-vue'
+        'stylelint-config-standard',
+        'stylelint-config-standard-vue'
     ]
 } as Config;
