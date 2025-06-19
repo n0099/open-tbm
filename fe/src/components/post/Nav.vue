n0099/open-tbm

commit e9579e6cb033ef263fcf2bc4f61aa9539c5c35a2

* replace all implicitly usages of lodash iteratee shorthands like `_.matches` & `_.property` with plain predicate for better typing: https://stackoverflow.com/questions/43384596/can-you-please-explain-lodash-iteratee-function-in-english @ fe

fe/src/pages/bilibiliVote.vue
@@ -176,10 +176,9 @@ const voteCountSeriesLabelFormatter = (
 ) => {
     const [timeline] = echartsInstances.top10CandidatesTimeline?.getOption()
         .timeline as [{ data: number[], currentIndex: number }];
-    const previousTimelineValue = _.find(votesData, {
-        endTime: timeline.data[timeline.currentIndex - 1],
-        voteFor: Number(candidateIndex.slice(0, Math.max(0, candidateIndex.indexOf('号')))) // trim trailing '号' in series name
-    });
+    const previousTimelineValue = _.find(votesData, vote =>
+        vote.endTime === timeline.data[timeline.currentIndex - 1]
+        && vote.voteFor === Number(candidateIndex.slice(0, Math.max(0, candidateIndex.indexOf('号'))))); // trim trailing '号' in series name
 
     return `${currentCount} (+${currentCount - (previousTimelineValue?.count ?? 0)})`;
 };
@@ -466,7 +465,7 @@ interface Coord { coord: [number, number] }
 type DiffWithPreviousMarkLineFormatter =
     Array<[Coord & { label: { show: true, position: 'middle', formatter: string } }, Coord]>;
 const findVoteCount = (votes: Array<{ isValid: IsValid, count: number }>, isValid: IsValid) =>
-    _.find(votes, { isValid })?.count ?? 0;
+    _.find(votes, vote => vote.isValid === isValid)?.count ?? 0;
 const formatCandidateName = (id: number) => `${id}号\n${json.candidateNames[id - 1]}`;
 
 // eslint-disable-next-line capitalized-comments
@@ -483,10 +482,10 @@ const chartLoadder = {
             .groupBy('voteFor')
 
             // sort grouped candidate by its descending total votes count
-            .sortBy(candidate => -_.sumBy(candidate, 'count'))
+            .sortBy(candidates => -_.sumBy(candidates, candidate => candidate.count))
             .map(candidateVotes => {
-                const validVotes = _.find(candidateVotes, { isValid: 1 });
-                const invalidVotes = _.find(candidateVotes, { isValid: 0 });
+                const validVotes = _.find(candidateVotes, vote => vote.isValid === 1);
+                const invalidVotes = _.find(candidateVotes, vote => vote.isValid === 0);
                 const officialValidCount = _.find(json.top50CandidatesOfficialValidVoteCount,
                     { voteFor: candidateVotes[0].voteFor })
                     ?.officialValidCount ?? 0;
@@ -502,7 +501,7 @@ const chartLoadder = {
             })
             .value();
 
-        const validCount = _.map(dataset, 'validCount');
+        const validCount = _.map(dataset, i => i.validCount);
         const validCountDiffWithPrevious: DiffWithPreviousMarkLineFormatter = validCount.map((count, index) => [{
             label: {
                 show: true,
@@ -527,12 +526,12 @@ const chartLoadder = {
     },
     top10CandidatesTimeline() {
         top10CandidatesTimelineVotes = {
-            valid: _.filter(json.top10CandidatesTimeline, { isValid: 1 }),
-            invalid: _.filter(json.top10CandidatesTimeline, { isValid: 0 })
+            valid: _.filter(json.top10CandidatesTimeline, vote => vote.isValid === 1),
+            invalid: _.filter(json.top10CandidatesTimeline, vote => vote.isValid === 0)
         };
 
         const options: ChartOptionTop10CandidatesTimeline[] = [];
-        _.each(_.groupBy(json.top10CandidatesTimeline, 'endTime'), (timeGroup, time) => {
+        _.each(_.groupBy(json.top10CandidatesTimeline, vote => vote.endTime), (timeGroup, time) => {
             const dataset: Top10CandidatesTimelineDataset = _.chain(timeGroup)
                 .sortBy('count')
                 .groupBy('voteFor')
@@ -545,7 +544,7 @@ const chartLoadder = {
                 }))
                 .value();
 
-            const validCount = _.map(dataset, 'validCount');
+            const validCount = _.map(dataset, i => i.validCount);
             const validCountDiffWithPrevious: DiffWithPreviousMarkLineFormatter =
                 (validCount.map((count, index) => [
                     {
@@ -602,7 +601,7 @@ const chartLoadder = {
         const originalTimelineOptions = structuredClone(options.at(-1));
         if (originalTimelineOptions === undefined || !_.isArray(originalTimelineOptions.series))
             return;
-        _.remove(originalTimelineOptions.series, { id: 'totalVotesValidation' });
+        _.remove(originalTimelineOptions.series, serie => serie.id === 'totalVotesValidation');
         options.push(_.merge(originalTimelineOptions, { // deep merge
             dataset: {
                 source: _.chain(json.top50CandidatesOfficialValidVoteCount)
@@ -655,8 +654,8 @@ const chartLoadder = {
             : json.top5CandidatesVoteCountGroupByHour;
         const top5CandidatesIndex = _.chain(top5CandidateCountGroupByTime)
             .filter({ isValid: 1 }).map('voteFor').sort().sortedUniq().value(); // not order by votes count
-        const validVotes = _.filter(top5CandidateCountGroupByTime, { isValid: 1 });
-        const invalidVotes = _.filter(top5CandidateCountGroupByTime, { isValid: 0 });
+        const validVotes = _.filter(top5CandidateCountGroupByTime, vote => vote.isValid === 1);
+        const invalidVotes = _.filter(top5CandidateCountGroupByTime, vote => vote.isValid === 0);
         const series: LineSeriesOption[] = [];
         top5CandidatesIndex.forEach(candidateIndex => {
             series.push({
@@ -664,7 +663,7 @@ const chartLoadder = {
                 type: 'line',
                 symbolSize: 2,
                 smooth: true,
-                data: _.filter(validVotes, { voteFor: candidateIndex }).map(i => [i.time, i.count])
+                data: _.filter(validVotes, vote => vote.voteFor === candidateIndex).map(i => [i.time, i.count])
             }, {
                 name: `${candidateIndex}号无效票增量`,
                 type: 'line',
@@ -672,7 +671,7 @@ const chartLoadder = {
                 smooth: true,
                 xAxisIndex: 1,
                 yAxisIndex: 1,
-                data: _.filter(invalidVotes, { voteFor: candidateIndex }).map(i => [i.time, i.count])
+                data: _.filter(invalidVotes, vote => vote.voteFor === candidateIndex).map(i => [i.time, i.count])
             });
         });
         echartsInstances.top5CandidateCountGroupByTime?.setOption({
@@ -716,7 +715,7 @@ candidatesDetailData.value = Object.values(_.merge(
         .map((candidateName, index) =>
             ({ candidateIndex: index + 1, candidateName, officialValidCount: null, validCount: 0, invalidCount: 0 }))
         .map(candidate => {
-            const candidateVotes = _.filter(json.allCandidatesVoteCount, { voteFor: candidate.candidateIndex });
+            const candidateVotes = _.filter(json.allCandidatesVoteCount, vote => vote.voteFor === candidate.candidateIndex);
 
             return {
                 ...candidate,

fe/src/utils/user.ts
@@ -11,7 +11,7 @@ export const toUserRoute = (uid: BaiduUserID): LocationAsRelativeRaw =>
     ({ name: 'users/uid', params: { uid } });
 
 export const baseGetUser = (users: User[]) => {
-    const usersKeyByUid = _.mapKeys(users, 'uid');
+    const usersKeyByUid = _.mapKeys(users, user => user.uid);
 
     return (uid: BaiduUserID): User => (usersKeyByUid[uid] as User | undefined) ?? {
         uid: 0,
@@ -30,7 +30,7 @@ export const baseGetUser = (users: User[]) => {
     };
 };
 export const baseGetLatestReplier = (latestRepliers: LatestReplier[]) => {
-    const latestRepliersKeyById = _.mapKeys(latestRepliers, 'id');
+    const latestRepliersKeyById = _.mapKeys(latestRepliers, latestReplier => latestReplier.id);
 
     return (id: LatestReplierId | null): LatestReplier | undefined =>
         (id === null ? undefined : latestRepliersKeyById[id]);

commit 4f657d205a2fb39088c56c5808b4b1ac6d6fa342

* fix the callback for intersection observer on replies will never trigger except the first once it started observe with high DPI scaling due to subpixels on Firefox * fix only starts to observe intersection for replies after 5 seconds @ `stores/viewportTopmostPost.ts`
* add param `cursor` for `menuReplyClasses()` to match reply more strictly with cursor and tid @ `<PostNav>`
* replace css shorthand prop `backgroup` with longhand `background-image` & `background-color` for overriding on `.reply-title` @ `<PostRendererListReply>`
* fix typo of `serie` in `chartLoadder.top10CandidatesTimeline()` that introduced in e9579e6cb033ef263fcf2bc4f61aa9539c5c35a2 @ `pages/bilibiliVote/vue`
@ fe

fe/src/components/post/Nav.vue
@@ -18,7 +18,7 @@
                         <template v-for="reply in thread.replies" :key="reply.pid">
                             <NuxtLink
                                 @click.prevent="navigate(cursor, reply)" :to="routeHash(reply)"
-                                :class="menuReplyClasses(reply)" class="post-nav-reply btn ms-0 px-2">
+                                :class="menuReplyClasses(cursor, reply)" class="post-nav-reply btn ms-0 px-2">
                                 {{ reply.floor }}L
                             </NuxtLink>
                         </template>
@@ -116,12 +116,14 @@ const menuThreadClasses = (thread: Thread) => {
     };
     /* eslint-enable @typescript-eslint/naming-convention */
 };
-const menuReplyClasses = (reply: Reply) => {
+const menuReplyClasses = (cursor: Cursor, reply: Reply) => {
     if (hydrationStore.isHydrating)
         return 'btn-light text-body-secondary';
     const isRouteHash = route.hash === routeHash(reply);
     const isHighlighting = highlightPostStore.isHighlightingPost(reply, 'pid');
-    const isTopmost = reply.pid === viewportTopmostPost.value?.pid;
+    const isTopmost = viewportTopmostPost.value?.cursor === cursor
+        && viewportTopmostPost.value.tid === reply.tid
+        && viewportTopmostPost.value.pid === reply.pid;
 
     return { /* eslint-disable @typescript-eslint/naming-convention */
         ...keysWithSameValue(['rounded-3', 'btn-info', 'text-white'], isTopmost),

fe/src/components/post/renderer/list/Reply.vue
@@ -89,7 +89,8 @@ const { stickyTitleEl } = await viewportTopmostPostStore.observe(
     margin-block-start: v-bind('replyTitleStyle.marginBlockStart.remString');
     border-block-start: 1px solid #ededed;
     border-block-end: 0;
-    background: linear-gradient(rgba(237, 237, 237, 1), rgba(237, 237, 237, .1));
+    background-color: #0000;
+    background-image: linear-gradient(rgba(237, 237, 237, 1), rgba(237, 237, 237, .1));
     /* https://stackoverflow.com/questions/25308823/targeting-positionsticky-elements-that-are-currently-in-a-stuck-state/79471060#79471060 */
     container: reply-title / scroll-state;
 }

fe/src/pages/bilibiliVote.vue
@@ -601,7 +601,7 @@ const chartLoadder = {
         const originalTimelineOptions = structuredClone(options.at(-1));
         if (originalTimelineOptions === undefined || !_.isArray(originalTimelineOptions.series))
             return;
-        _.remove(originalTimelineOptions.series, serie => serie.id === 'totalVotesValidation');
+        _.remove(originalTimelineOptions.series, series => series.id === 'totalVotesValidation');
         options.push(_.merge(originalTimelineOptions, { // deep merge
             dataset: {
                 source: _.chain(json.top50CandidatesOfficialValidVoteCount)

fe/src/stores/viewportTopmostPost.ts
@@ -81,16 +81,18 @@ export const useViewportTopmostPostStore = defineStore('viewportTopmostPost', ()
                     } else {
                         stopExistingWindowHeightWatcher();
                         let stopExistingObserver = noop;
-                        stopExistingWindowHeightWatcher = watchDebounced(windowHeight, () => {
+                        const reObserve = () => {
                             stopExistingObserver();
 
                             // bottom: additional +topOffset and not using -100% to fix https://bugzilla.mozilla.org/show_bug.cgi?id=1918017
                             // top: -topOffset will move down the trigger line below the top border to match with its offset
-                            const rootMargin = `${-topOffset}px 0px ${-windowHeight.value + topOffset}px 0px`;
+                            const rootMargin = `${-topOffset}px 0px ${-windowHeight.value + topOffset + 1}px 0px`;
                             observerForNonZeroTopOffset = new IntersectionObserver(onIntersect, { rootMargin });
                             observerForNonZeroTopOffset.observe(currentTarget);
                             stopExistingObserver = () => { observerForNonZeroTopOffset.disconnect() };
-                        }, { debounce: 5000, immediate: true });
+                        };
+                        reObserve(); // `{ immediate: true }` option will trigger the callback immediate after the value of `debounce` ms passed
+                        stopExistingWindowHeightWatcher = watchDebounced(windowHeight, reObserve, { debounce: 5000 });
                     }
                 };
             }, { flush: 'post' });