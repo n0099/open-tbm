c34c1a6671ed673c5206b28e6c493e26c5257a4a
n0099 <n@n0099.net>
2025-08-13 14:03:54 +0000

    * replace usage of `Doctrine\DBAL\Platforms\AbstractPlatform::quoteIdentifier()` with `quoteSingleIdentifier()` that deprecated in https://github.com/doctrine/dbal/pull/6590 @ `App\Doctrine\AlwaysQuoteStrategy` @ be

--- be/src/Doctrine/AlwaysQuoteStrategy.php
+++ be/src/Doctrine/AlwaysQuoteStrategy.php
@@ -10,6 +10,6 @@ class AlwaysQuoteStrategy extends DefaultQuoteStrategy
 {
     public function getColumnName(string $fieldName, ClassMetadata $class, AbstractPlatform $platform): string
     {
-        return $platform->quoteIdentifier($class->fieldMappings[$fieldName]->columnName);
+        return $platform->quoteSingleIdentifier($class->fieldMappings[$fieldName]->columnName);
     }
 }
