41b1800bb135339f5f065fa0c71f07db1894ff80
n0099 <n@n0099.net>
2025-08-19 23:36:55 +0000

    * merge `plugins/directive.server.ts` into `stores/viewer.ts` * add margin around the left panel @ `<OgImagePost>` * replace the pattern of `watch(isX, () => if (isX))` with `whenever()` from vueuse @ `pages/posts.vue` * add script `tsc` like 70004b30629d58ac9c9da0633073114cfcb7b157 @ package.json @ fe

--- fe/src/plugins/directive.server.ts
+++
@@ -1,6 +0,0 @@
-// https://github.com/nuxt/nuxt/issues/13382#issuecomment-2192801116
-export default defineNuxtPlugin(nuxt => {
-    if (import.meta.dev)
-        return;
-    nuxt.vueApp.directive('viewer', {});
-});
