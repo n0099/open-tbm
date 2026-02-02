646bcf9b3eb626c90b1e3052bcdbe40daaf3a4c5
n0099 <git@n0099.com>
2026-02-02 17:34:00 +0000

    + `php-fpm` and `nginx` @ be * fix replacing `process-compose` with `exec zsh` when lanunching with `devenv up` @ devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   cs = {
@@ -22,8 +27,51 @@ let
         extensions = { enabled, all }: enabled ++ [ all.xdebug ];
         extraConfig = "xdebug.mode=debug";
       };
+      fpm.pools.www.settings =
+        let
+          nproc = pkgs.runCommandLocal "nproc" { } "nproc > $out" |> lib.readFile |> lib.toIntBase10;
+        in
+        {
+          pm = "dynamic";
+          "pm.max_children" = nproc * 2;
+          "pm.min_spare_servers" = 1;
+          "pm.max_spare_servers" = nproc;
+        };
     };
     enterShell = /* sh */ "(cd be && composer i)";
+    services.nginx = {
+      enable = true;
+      httpConfig = ''
+        server {
+          listen 8080;
+          server_name localhost;
+          index index.php index.html;
+
+          # https://github.com/nginxinc/nginx-wiki/blob/836ecd605a1b9861fb608e848336bca9b8640b54/source/start/topics/examples/phpfcgi.rst
+          location ~ [^/]\.php(/|$) {
+              fastcgi_index index.php;
+              fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+              fastcgi_pass unix:${config.env.PHPFPMDIR}/www.sock; # https://github.com/cachix/devenv/blob/a208bf67ac2874fec086aa94cfdead0f40de3613/src/modules/languages/php.nix#L155
+              add_header X-Powered-By php always;
+
+              # https://serverfault.com/questions/627903/is-the-php-option-cgi-fix-pathinfo-really-dangerous-with-nginx-php-fpm
+              if (!-f $request_filename) {
+                  return 404;
+              }
+              try_files $uri =404;
+
+              # https://httpoxy.org
+              fastcgi_param HTTP_PROXY "";
+
+              include ${pkgs.nginx}/conf/fastcgi_params;
+              # https://github.com/nginxinc/nginx-wiki/blob/836ecd605a1b9861fb608e848336bca9b8640b54/source/start/topics/tutorials/config_pitfalls.rst#use-request_filename-for-script_filename
+              fastcgi_param SCRIPT_FILENAME $request_filename;
+              # https://serverfault.com/questions/465607/nginx-document-rootfastcgi-script-name-vs-request-filename
+              # fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+          }
+        }
+      '';
+    };
   };
   fe = {
     languages = {
@@ -62,7 +110,10 @@ lib.mkMerge [
     enterShell = /* sh */ ''
       git submodule init
       git submodule update
-      command -v zsh > /dev/null && exec zsh
+      if [ $DEVENV_CMDLINE != up ] && command -v zsh > /dev/null
+      then
+        exec zsh
+      fi
     '';
   }
 ]
