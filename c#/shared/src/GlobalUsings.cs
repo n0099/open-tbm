12c6d85f1495888cb78224c31be6b8fe25f21287
n0099 <git@n0099.com>
2026-02-27 06:26:49 +0000

    * fix violation of Roslyn analyzer rule https://github.com/DotNetAnalyzers/StyleCopAnalyzers/blob/master/documentation/SA1208.md @ shared/src/GlobalUsings.cs * enable `TreatWarningsAsErrors` in all `Configuration`s instead of only `Release` @ Directory.Build.props @ c#

--- c#/shared/src/GlobalUsings.cs
+++ c#/shared/src/GlobalUsings.cs
@@ -1,7 +1,7 @@
+global using System.Diagnostics.CodeAnalysis;
 global using Autofac;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using NLog.Extensions.Logging;
 global using Npgsql;
-global using System.Diagnostics.CodeAnalysis;
